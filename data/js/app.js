db863a95bca33535da7c4813d82922676b0e1a77
cedricalfonsi <cedricalfonsi@gmail.com>
2015-03-29 07:29:53 +0530

    PASSBOLT-832 Passbolt plugin share

--- data/js/app.js
+++ data/js/app.js
@@ -26,7 +26,7 @@ passbolt.message('passbolt.keyring.master.request')
 
 // Decrypt a password when the user is clicking on a pwd cell of the password browser component.
 // Copy it in the clipboard.
-$('body').on('click', '#js_wsp_pwd_browser .password',  function() {
+$('body').on('click', '#js_wsp_pwd_browser .password', function() {
   var armoredSecret = $('pre', this).html();
   // Decrypt the armored secret.
   passbolt.cipher.decrypt(armoredSecret)
@@ -51,6 +51,30 @@ window.addEventListener('passbolt.secret_edition.encrypt', function(event) {
       var armoreds = [armoreds];
       passbolt.event.triggerToPage('secret_edition_secret_encrypted', armoreds);
     });
+});
+
+// When the user wants to share a password with other people.
+// secret for the users the resource is shared with.
+// Dispatch this event to the secret edition iframe which will take care of the encryption.
+window.addEventListener('passbolt.resource_share.encrypt', function(event) {
+  var data = event.detail,
+    resourceId = data.resourceId,
+    userId = data.userId;
+
+  var url = self.options.config.url + '/resources/' + resourceId + '.json';
+  $.get(url, function(responseRaw) {
+    var response = JSON.parse(responseRaw);
+    if (response) {
+      resource = response.body;
+      passbolt.cipher.decrypt(resource.Secret[0].data)
+        .then(function(secret) {
+          passbolt.cipher.encrypt(secret, [userId])
+            .then(function(armoreds) {
+              passbolt.event.triggerToPage('resource_share_secret_encrypted', armoreds);
+            });
+        });
+    }
+  });
 });
 
 // Listen when a resource is edited and inject the passbolt secret field component.

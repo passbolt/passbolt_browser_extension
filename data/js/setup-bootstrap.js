a92941f98a7a5bf107d9e05ad97a7e9a4533de9f
cedricalfonsi <cedricalfonsi@gmail.com>
2015-04-20 14:36:18 +0530

    PASSBOLT-908 Bootstrap setup, redirect to the plugin has been detected page

--- data/js/setup-bootstrap.js
+++ data/js/setup-bootstrap.js
@@ -7,6 +7,9 @@
   // the wizard installer. The step which is provided by the backend of passbolt.
   domain = location.substr(0, location.indexOf(self.options.config.setupBootstrapUrl));
 
-  // Redirect the user to the second page of the wizard.
-  self.port.emit('passbolt.install.plugin_detected', domain);
+  // Notify the add-on that the user perform a plugin check operation, either by clicking
+  // on the retry button after he installed the plugin, or loading the server setup bootstrap
+  // with the plugin already installed.
+  // What will redirect him to the second step of the wizard.
+  self.port.emit('passbolt.setup.plugin_check', domain);
 })();

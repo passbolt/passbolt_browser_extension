2cc5ee0d3a9e65a51f30b9299f8366d64341ac76
remy <remy@stripthis.com>
2015-10-22 18:46:04 +0530

    No config and stage0

--- data/js/login.js
+++ data/js/login.js
@@ -3,10 +3,15 @@ var passbolt = passbolt || {};
 
 $(window).load(function() {
 
-    passbolt.login.onConfigurationMissing = function() {
+    /**
+     * Update the login page with a server side rendered template
+     * @param step
+     */
+    passbolt.login.render = function(step) {
+        console.log('render' + step);
         var context = $('.login.page .js_main-login-section');
         $.ajax({
-            url: '/auth/partials/register',
+            url: '/auth/partials/' + step,
             context: context
         }).done(function(data) {
             $( this ).html(data);
@@ -15,10 +20,40 @@ $(window).load(function() {
         });
     };
 
-    if (self.options.ready === true) {
-        console.log('login possible!');
-    } else {
-        passbolt.login.onConfigurationMissing();
-    }
+    /**
+     * When the plugin configuration is missing
+     */
+    passbolt.login.onConfigurationMissing = function() {
+        // Do not allow login, but explain you need to register
+        // or contact the domain administrator based on server side config
+        passbolt.login.render('noconfig');
+        console.log('onConfigurationMissing');
+    };
+
+    /**
+     * Starts with server key check
+     */
+    passbolt.login.onStep0Start = function() {
+        // Display a informations about the state of login
+        // e.g. that we're going to check for the server key first
+        passbolt.login.render('stage0');
+
+    };
+
+    passbolt.login.onStep0CheckServerKey = function () {
+        console.log('onstep0 check server key');
+    };
+
+    /**
+     * Check if the addon says we are ready for login
+     */
+    passbolt.login.init = function() {
+        if (self.options.ready === true) {
+            passbolt.login.onStep0Start();
+        } else {
+            passbolt.login.onConfigurationMissing();
+        }
+    };
+    passbolt.login.init();
 
 });

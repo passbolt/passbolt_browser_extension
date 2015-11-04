48ce292736ca954632b53e39252af5c12f16e85d
Kevin Muller <mynameisgnu@gmail.com>
2015-11-04 22:33:20 +0530

    #PASSBOLT-919 full login process

--- data/js/inc/setup/password.js
+++ data/js/inc/setup/password.js
@@ -9,125 +9,45 @@ passbolt.setup.steps = passbolt.setup.steps || {};
 
   var step = {
     'id': 'password',
-    'title' : 'Alright sparky, let\'s create your first password!',
+    'title' : 'Alright sparky, it\'s time to log in!',
     'label': '5. Login !',
     'parents': ['security_token'],
     'viewData': {}
   };
 
   step.init = function() {
-    step.viewData.firstName = passbolt.setup.data.firstName || null;
-    step.viewData.lastName = passbolt.setup.data.lastName || null;
-    step.viewData.domain = passbolt.setup.data.domain || null;
-    step.viewData.username = passbolt.setup.data.username || null;
+
   };
 
   step.start = function() {
     // Disable submit button at the beginning.
-    passbolt.setup.setActionState('submit', 'disabled');
-
-    // Declare elements and variables.
-    var $password = $('#js_setup_password'),
-      $passwordClear = $('#js_setup_password_clear'),
-      $viewPasswordButton = $('#js_show_pwd_button'),
-      $passwordStrength = $('#js_user_pwd_strength'),
-      $generatePasswordButton = $('#js_gen_pwd_button');
-
-    /**
-     * show / hide the secret.
-     */
-    var toggleViewPassword = function() {
-      if ($password.hasClass('hidden')) {
-        $password.removeClass('hidden');
-        $passwordClear.addClass('hidden');
-        $viewPasswordButton.removeClass('selected');
-      } else {
-        $password.addClass('hidden');
-        $passwordClear.removeClass('hidden');
-        $passwordClear.val($password.val());
-        $viewPasswordButton.addClass('selected');
-      }
-    };
-
-    /**
-     * Update the secret strength component.
-     * @param secret
-     */
-    var updatePasswordStrength = function(password) {
-      getTpl('./tpl/secret/strength.ejs', function(tpl) {
-        var strength = secretComplexity.strength(password);
-        var data = {
-          strengthId: secretComplexity.STRENGTH[strength].id,
-          strengthLabel: secretComplexity.STRENGTH[strength].label
-        };
-        $passwordStrength.html(new EJS({text: tpl}).render(data));
-      });
-    };
-
-    // On input change.
-    $password.on('input change', function() {
-      var password = $password.val();
-      // Update password in clear.
-      $passwordClear.val(password);
-      // Update strength.
-      updatePasswordStrength(password);
-
-      // The submit button will be enabled only if the password
-      // contains more than 8 characters.
-      if($password.val().length >= 8) {
-        passbolt.setup.setActionState('submit', 'enabled');
-      } else {
-        passbolt.setup.setActionState('submit', 'disabled');
-      }
-    });
-
-    // When the clear password is updated.
-    $passwordClear.on('input', function() {
-      // Update password field.
-      $password.val($passwordClear.val())
-        .trigger('change');
-    });
-
-    // When the generate a new password button is clicked.
-    $generatePasswordButton.on('click', function(ev) {
-      ev.preventDefault();
-      // Generate a new password with secret class.
-      $password.val(secretComplexity.generate())
-        .trigger('change');
-    });
-
-    // When the user explicitly wants to view the password.
-    $viewPasswordButton.on('click', function(ev) {
-      ev.preventDefault();
-      toggleViewPassword();
-    });
+    passbolt.setup.setActionState('submit', 'processing');
+      step.submit();
   };
 
-  step.autologin = function() {
-    getTpl('./tpl/setup/login_form.ejs', function(tpl) {
-      var data = {
-        login_url : passbolt.setup.data.domain + "/login",
-        username: passbolt.setup.data.username,
-        password: passbolt.setup.data.password
-      };
-      // Renders an invisible form.
-      $('#SetupAutoLogin').html(new EJS({text: tpl}).render(data));
-      // Submit form.
-      $('form', $('#SetupAutoLogin')).submit();
-    });
+  step.goToLogin = function() {
+      var loginUrl = passbolt.setup.data.domain + "/login";
+      setTimeout(
+          function() {
+              window.location.href = loginUrl;
+          },
+          1000);
   };
 
   // Submit step.
   step.submit = function() {
-    passbolt.setup.setActionState('submit', 'processing');
     var def = $.Deferred();
 
-    passbolt.setup.data.password = $('#js_setup_password').val();
+      // TODO : change that, and remove password from entry point.
+    passbolt.setup.data.password = 'password';
     passbolt.request('passbolt.setup.save', passbolt.setup.data)
-      .then(function() {
-        // Autologin.
-        step.autologin();
-      });
+        .then(function() {
+            // Autologin.
+            step.goToLogin();
+        })
+        .fail(function() {
+            alert("could not save information");
+        });
 
     return def;
   };

3cf717a35d88a6f44f5c08af54e92768c74085ea
remy <remy@stripthis.com>
2015-11-20 14:52:02 +0530

    Fixing boostrap.js + Styleguide 0.3.0

--- data/js/bootstrap.js
+++ data/js/bootstrap.js
@@ -1,4 +1,41 @@
-// Modify html classes to signify plugin presence.
-$('html')
-    .removeClass('no-passboltplugin')
-    .addClass('passboltplugin');
+var passbolt = passbolt || {};
+
+(function($) {
+
+	// check if the plugin is configured
+	passbolt.request('passbolt.addon.isConfigured')
+		.then(function (response) {
+			if (response !== true) {
+				$('html')
+					.addClass('no-passboltplugin-config')
+					.removeClass('passboltplugin-config');
+			} else {
+				$('html')
+					.addClass('passboltplugin-config')
+					.removeClass('no-passboltplugin-config');
+			}
+		});
+
+	// Add classes relative to plugin.
+	$('html')
+		.removeClass('no-passboltplugin')
+		.addClass('passboltplugin');
+
+	// check if it is a passbolt app instance on the login page
+	if($('html.passbolt .login.page').length) {
+		passbolt.request('passbolt.bootstrap.login')
+			.then(
+			function success(refresh) {
+				if (refresh) {
+					location.reload();
+				}
+			}
+		);
+	}
+
+	// check if it is a passbolt app instance on the debug page
+	if($('html.passbolt .debug.page').length) {
+		passbolt.request('passbolt.bootstrap.debug');
+	}
+
+})(jQuery);

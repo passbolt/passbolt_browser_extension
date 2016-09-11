0ff465dbf63ada42177158f465cfd83a36de5c42
Cedric Alfonsi <cedricalfonsi@gmail.com>
2016-09-11 16:00:49 +0200

    PASSBOLT-1660 Checkstyle

--- data/js/bootstrap.js
+++ data/js/bootstrap.js
@@ -8,87 +8,87 @@
 var passbolt = passbolt || {};
 passbolt.bootstrap = passbolt.bootstrap || {};
 
-(function($) {
+(function ($) {
 
-    /**
-     * When the domain is not the right one, but the plugin is already configured.
-     */
-    passbolt.bootstrap.onWrongDomain = function() {
-        $('html').addClass('domain-unknown');
+  /**
+   * When the domain is not the right one, but the plugin is already configured.
+   */
+  passbolt.bootstrap.onWrongDomain = function () {
+    $('html').addClass('domain-unknown');
 
-        var $renderSpace = $('.login.page .js_main-login-section'),
-            publicRegistration = $('.login.page.public-registration').length > 0 ? true : false;
+    var $renderSpace = $('.login.page .js_main-login-section'),
+      publicRegistration = $('.login.page.public-registration').length > 0 ? true : false;
 
-        // Get trusted domain setting.
-        passbolt.request('passbolt.addon.getDomain').then(
-            function success(trustedDomain) {
-                // Get template.
-                passbolt.helper.html.loadTemplate($renderSpace, './tpl/login/wrong-domain.ejs','html', {
-                  trustedDomain : trustedDomain,
-                  publicRegistration : publicRegistration
-                });
-            });
-    };
-
-    // Get plugin version and add it in the footer.
-    passbolt.request('passbolt.addon.getVersion')
-        .then(function(version) {
-            var $versionElt = $('#version > a');
-            var appVersion = $versionElt.attr('data-tooltip');
-            $versionElt.attr('data-tooltip', (appVersion + ' / ' + version));
+    // Get trusted domain setting.
+    passbolt.request('passbolt.addon.getDomain').then(
+      function success(trustedDomain) {
+        // Get template.
+        passbolt.helper.html.loadTemplate($renderSpace, './tpl/login/wrong-domain.ejs', 'html', {
+          trustedDomain: trustedDomain,
+          publicRegistration: publicRegistration
         });
+      });
+  };
 
-	// check if the plugin is configured
-	passbolt.request('passbolt.addon.isConfigured')
-		.then(function (response) {
-			if (response !== true) {
-				$('html')
-					.addClass('no-passboltplugin-config')
-					.removeClass('passboltplugin-config');
-			} else {
-				$('html')
-					.addClass('passboltplugin-config')
-					.removeClass('no-passboltplugin-config');
-			}
-		});
+  // Get plugin version and add it in the footer.
+  passbolt.request('passbolt.addon.getVersion')
+    .then(function (version) {
+      var $versionElt = $('#version > a');
+      var appVersion = $versionElt.attr('data-tooltip');
+      $versionElt.attr('data-tooltip', (appVersion + ' / ' + version));
+    });
 
-	// Add classes relative to plugin.
-	$('html')
-		.removeClass('no-passboltplugin')
-		.addClass('passboltplugin');
+  // check if the plugin is configured
+  passbolt.request('passbolt.addon.isConfigured')
+    .then(function (response) {
+      if (response !== true) {
+        $('html')
+          .addClass('no-passboltplugin-config')
+          .removeClass('passboltplugin-config');
+      } else {
+        $('html')
+          .addClass('passboltplugin-config')
+          .removeClass('no-passboltplugin-config');
+      }
+    });
 
-	// check if it is a passbolt app instance on the login page
-	if($('html.passbolt .login.page').length) {
-        // If passbolt is configured.
-        passbolt.request('passbolt.addon.isConfigured')
-            .then(function (response) {
-                if (response === true) {
-                    // If domain is right.
-                    passbolt.request('passbolt.addon.checkDomain').then(
-                        function success(response) {
-                            // Domain not right, we redirect to wrong domain page.
-                            if (response !== true) {
-                                passbolt.bootstrap.onWrongDomain();
-                                return;
-                            }
+  // Add classes relative to plugin.
+  $('html')
+    .removeClass('no-passboltplugin')
+    .addClass('passboltplugin');
 
-                            // Domain is right, we go to login.
-                            passbolt.request('passbolt.bootstrap.login').then(
-                                function success(refresh) {
-                                    if (refresh) {
-                                        location.reload();
-                                    }
-                                }
-                            );
-                        }
-                    );
+  // check if it is a passbolt app instance on the login page
+  if ($('html.passbolt .login.page').length) {
+    // If passbolt is configured.
+    passbolt.request('passbolt.addon.isConfigured')
+      .then(function (response) {
+        if (response === true) {
+          // If domain is right.
+          passbolt.request('passbolt.addon.checkDomain').then(
+            function success(response) {
+              // Domain not right, we redirect to wrong domain page.
+              if (response !== true) {
+                passbolt.bootstrap.onWrongDomain();
+                return;
+              }
+
+              // Domain is right, we go to login.
+              passbolt.request('passbolt.bootstrap.login').then(
+                function success(refresh) {
+                  if (refresh) {
+                    location.reload();
+                  }
                 }
-            });
-	}
+              );
+            }
+          );
+        }
+      });
+  }
 
-	// check if it is a passbolt app instance on the debug page
-	if($('html.passbolt .debug.page').length) {
-		passbolt.request('passbolt.bootstrap.debug');
-	}
+  // check if it is a passbolt app instance on the debug page
+  if ($('html.passbolt .debug.page').length) {
+    passbolt.request('passbolt.bootstrap.debug');
+  }
 
 })(jQuery);
--- data/js/inc/setup/secret.js
+++ data/js/inc/setup/secret.js
@@ -10,181 +10,181 @@ passbolt.setup.steps = passbolt.setup.steps || {};
 
 (function (passbolt) {
 
-    var step = {
-        'id': 'secret',
-        'elts' : {
-            password : '#js_field_password',
-            passwordClear : '#js_field_password_clear',
-            viewPasswordButton : '#js_show_pwd_button',
-            passwordStrength : '#js_user_pwd_strength',
-            passwordCriterias : '#js_password_match_criterias'
-        }
-    };
-
-    /* ==================================================================================
-     *  Content code events
-     * ================================================================================== */
-
-    /**
-     * On click event on the view button.
-     *
-     * @param ev
-     * @param el
-     */
-    step.onViewButtonClick = function(ev, el) {
-        if (step.elts.$password.hasClass('hidden')) {
-            step.elts.$password.removeClass('hidden');
-            step.elts.$passwordClear.addClass('hidden');
-            step.elts.$viewPasswordButton.removeClass('selected');
-        } else {
-            step.elts.$password.addClass('hidden');
-            step.elts.$passwordClear.removeClass('hidden');
-            step.elts.$passwordClear.val(step.elts.$password.val());
-            step.elts.$viewPasswordButton.addClass('selected');
-        }
+  var step = {
+    'id': 'secret',
+    'elts': {
+      password: '#js_field_password',
+      passwordClear: '#js_field_password_clear',
+      viewPasswordButton: '#js_show_pwd_button',
+      passwordStrength: '#js_user_pwd_strength',
+      passwordCriterias: '#js_password_match_criterias'
     }
+  };
 
-    /**
-     * On input change event on the clear password field.
-     */
-    step.onPasswordClearInput = function() {
-        // Update password field.
-        step.elts.$password.val(step.elts.$passwordClear.val())
-            .trigger('change');
+  /* ==================================================================================
+   *  Content code events
+   * ================================================================================== */
+
+  /**
+   * On click event on the view button.
+   *
+   * @param ev
+   * @param el
+   */
+  step.onViewButtonClick = function (ev, el) {
+    if (step.elts.$password.hasClass('hidden')) {
+      step.elts.$password.removeClass('hidden');
+      step.elts.$passwordClear.addClass('hidden');
+      step.elts.$viewPasswordButton.removeClass('selected');
+    } else {
+      step.elts.$password.addClass('hidden');
+      step.elts.$passwordClear.removeClass('hidden');
+      step.elts.$passwordClear.val(step.elts.$password.val());
+      step.elts.$viewPasswordButton.addClass('selected');
     }
+  }
 
-    /**
-     * On input change event on the password field.
-     */
-    step.onPasswordInput = function() {
-        // Get password from password field.
-        var password = step.elts.$password.val();
+  /**
+   * On input change event on the clear password field.
+   */
+  step.onPasswordClearInput = function () {
+    // Update password field.
+    step.elts.$password.val(step.elts.$passwordClear.val())
+      .trigger('change');
+  }
 
-        // Update password in clear.
-        step.elts.$passwordClear.val(password);
+  /**
+   * On input change event on the password field.
+   */
+  step.onPasswordInput = function () {
+    // Get password from password field.
+    var password = step.elts.$password.val();
 
-        // Update strength.
-        step._updatePasswordStrength(password);
+    // Update password in clear.
+    step.elts.$passwordClear.val(password);
 
-        // Update criterias.
-        step._updatePasswordCriterias(password);
+    // Update strength.
+    step._updatePasswordStrength(password);
 
-        // Validate key.
-        passbolt.request('passbolt.keyring.key.validate', {passphrase : password}, ['passphrase'])
-            .then(function() {
-                passbolt.setup.setActionState('submit', 'enabled');
-            })
-            .then(null, function(errorMessage, validationErrors) {
-                passbolt.setup.setActionState('submit', 'disabled');
-            })
-    }
+    // Update criterias.
+    step._updatePasswordCriterias(password);
 
-
-    /* ==================================================================================
-     *  Core functions (Implements()).
-     * ================================================================================== */
-
-    /**
-     * Implements init().
-     * @returns {*}
-     */
-     step.init = function () {
-        var def = $.Deferred();
-        def.resolve();
-        return def;
-     };
-
-    /**
-     * Implements start().
-     */
-    step.start = function () {
-        // Disable submit button at the beginning.
+    // Validate key.
+    passbolt.request('passbolt.keyring.key.validate', {passphrase: password}, ['passphrase'])
+      .then(function () {
+        passbolt.setup.setActionState('submit', 'enabled');
+      })
+      .then(null, function (errorMessage, validationErrors) {
         passbolt.setup.setActionState('submit', 'disabled');
+      })
+  }
 
-        // Update password criterias for empty password.
-        step._updatePasswordCriterias('');
 
-        // On input change.
-        step.elts.$password.on('input change', function () {
-            step.onPasswordInput();
-        });
+  /* ==================================================================================
+   *  Core functions (Implements()).
+   * ================================================================================== */
 
-        // When the clear password is updated.
-        step.elts.$passwordClear.on('input', function () {
-            step.onPasswordClearInput();
-        });
+  /**
+   * Implements init().
+   * @returns {*}
+   */
+  step.init = function () {
+    var def = $.Deferred();
+    def.resolve();
+    return def;
+  };
 
-        // When the user explicitly wants to view the password.
-        step.elts.$viewPasswordButton.on('click', function (ev) {
-            ev.preventDefault();
-            step.onViewButtonClick(ev);
-        });
+  /**
+   * Implements start().
+   */
+  step.start = function () {
+    // Disable submit button at the beginning.
+    passbolt.setup.setActionState('submit', 'disabled');
+
+    // Update password criterias for empty password.
+    step._updatePasswordCriterias('');
+
+    // On input change.
+    step.elts.$password.on('input change', function () {
+      step.onPasswordInput();
+    });
+
+    // When the clear password is updated.
+    step.elts.$passwordClear.on('input', function () {
+      step.onPasswordClearInput();
+    });
+
+    // When the user explicitly wants to view the password.
+    step.elts.$viewPasswordButton.on('click', function (ev) {
+      ev.preventDefault();
+      step.onViewButtonClick(ev);
+    });
+  };
+
+  /**
+   * Implements submit().
+   * @returns {*}
+   */
+  step.submit = function () {
+    passbolt.setup.setActionState('submit', 'processing');
+
+    // Validate the key and return promise related to validation.
+    return passbolt.request('passbolt.keyring.key.validate', {passphrase: step.elts.$password.val()}, ['passphrase'])
+      .then(function () {
+        passbolt.setup.set('key.passphrase', step.elts.$password.val());
+      })
+      .then(null, function (errorMessage, validationErrors) {
+        passbolt.setup.setActionState('submit', 'disabled');
+      })
+  };
+
+  /**
+   * Implements cancel().
+   * @returns {*}
+   */
+  step.cancel = function () {
+    passbolt.setup.setActionState('cancel', 'processing');
+    var def = $.Deferred();
+    def.resolve();
+    return def;
+  };
+
+  /* ==================================================================================
+   *  Business functions
+   * ================================================================================== */
+
+  /**
+   * Update the secret strength component.
+   * @param secret
+   */
+  step._updatePasswordCriterias = function (password) {
+    var criterias = {};
+    if (password.length > 0) {
+      var criterias = secretComplexity.matchMasks(password);
+      criterias['dictionary'] = null;
+      criterias['minLength'] = password.length >= 8;
+    }
+    var data = {
+      criterias: criterias
     };
 
-    /**
-     * Implements submit().
-     * @returns {*}
-     */
-    step.submit = function () {
-        passbolt.setup.setActionState('submit', 'processing');
+    passbolt.helper.html.loadTemplate(step.elts.$passwordCriterias, './tpl/secret/criterias.ejs', 'html', data);
+  };
 
-        // Validate the key and return promise related to validation.
-        return passbolt.request('passbolt.keyring.key.validate', {passphrase : step.elts.$password.val()}, ['passphrase'])
-            .then(function() {
-                passbolt.setup.set('key.passphrase', step.elts.$password.val());
-            })
-            .then(null, function(errorMessage, validationErrors) {
-                passbolt.setup.setActionState('submit', 'disabled');
-            })
+  /**
+   * Update the secret strength component.
+   * @param secret
+   */
+  step._updatePasswordStrength = function (password) {
+    var strength = secretComplexity.strength(password);
+    var data = {
+      strengthId: secretComplexity.STRENGTH[strength].id,
+      strengthLabel: secretComplexity.STRENGTH[strength].label
     };
 
-    /**
-     * Implements cancel().
-     * @returns {*}
-     */
-    step.cancel = function () {
-        passbolt.setup.setActionState('cancel', 'processing');
-        var def = $.Deferred();
-        def.resolve();
-        return def;
-    };
+    passbolt.helper.html.loadTemplate(step.elts.$passwordStrength, './tpl/secret/strength.ejs', 'html', data);
+  };
 
-    /* ==================================================================================
-     *  Business functions
-     * ================================================================================== */
-
-    /**
-     * Update the secret strength component.
-     * @param secret
-     */
-    step._updatePasswordCriterias = function (password) {
-        var criterias = {};
-        if (password.length > 0) {
-            var criterias = secretComplexity.matchMasks(password);
-            criterias['dictionary'] = null;
-            criterias['minLength'] = password.length >= 8;
-        }
-        var data = {
-            criterias: criterias
-        };
-
-        passbolt.helper.html.loadTemplate(step.elts.$passwordCriterias, './tpl/secret/criterias.ejs', 'html', data);
-    };
-
-    /**
-     * Update the secret strength component.
-     * @param secret
-     */
-    step._updatePasswordStrength = function (password) {
-        var strength = secretComplexity.strength(password);
-        var data = {
-            strengthId: secretComplexity.STRENGTH[strength].id,
-            strengthLabel: secretComplexity.STRENGTH[strength].label
-        };
-
-        passbolt.helper.html.loadTemplate(step.elts.$passwordStrength, './tpl/secret/strength.ejs', 'html', data);
-    };
-
-    passbolt.setup.steps[step.id] = step;
+  passbolt.setup.steps[step.id] = step;
 
 })(passbolt);

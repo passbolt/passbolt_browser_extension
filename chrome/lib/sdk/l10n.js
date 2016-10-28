97682bda718fcddf5ff0002dd724fff8f1a1a59b
Cedric Alfonsi <cedricalfonsi@gmail.com>
2016-10-28 10:06:40 +0200

    PASSBOLT-1725 remove unused sdk/tabs requires. Implement quick sdk/l10n

---
+++ chrome/lib/sdk/l10n.js
@@ -0,0 +1,17 @@
+/**
+ * l10n Chrome Wrapper
+ * Allow translating in chrome almost like firefox sdk
+ *
+ * @copyright (c) 2015-present Bolt Softwares Pvt Ltd
+ * @licence GNU Affero General Public License http://www.gnu.org/licenses/agpl-3.0.en.html
+ */
+
+/**
+ * Get the translation of a given text
+ * @param txt {string} The text to translate
+ * @return {string}
+ */
+var get = function(txt) {
+  return txt;
+};
+exports.get = get;
--- dist/chrome/lib/event/debugEvents.js
+++ dist/chrome/lib/event/debugEvents.js
@@ -4,7 +4,6 @@
  * @copyright (c) 2015-present Bolt Softwares Pvt Ltd
  * @licence GNU Affero General Public License http://www.gnu.org/licenses/agpl-3.0.en.html
  */
-var tabs = require('sdk/tabs');
 var Config = require('../model/config');
 var preferences = require("sdk/preferences/service");
 

6dc970cdbab177f1d7da716b0c0df7a0f100d08f
remy <remy@passbolt.com>
2017-07-10 17:02:52 +0530

    PASSBOLT-2092 Vendors should be served separately

--- src/all/lib/vendors.js
+++ src/all/lib/vendors.js
@@ -4,14 +4,17 @@
  * @copyright (c) 2017 Passbolt SARL
  * @licence GNU Affero General Public License http://www.gnu.org/licenses/agpl-3.0.en.html
  */
-var Validator = require('./vendors/validator');
-var _ = require('./vendors/underscore-min');
-var fetch = require('./vendors/window').fetch;
-var FormData = require('./vendors/window').FormData;
-const { urldecode, stripslashes, htmlspecialchars, in_array } = require('./vendors/phpjs');
-var storage = require('./vendors/node-localstorage').localStorage;
-var openpgp = require('./vendors/openpgp');
-var XRegExp = require('./vendors/xregexp').XRegExp;
-var jsSHA = require('./vendors/sha');
-var randomBytes = require('./vendors/crypto').randomBytes;
-var jsonQ = require('./vendors/jsonQ').jsonQ;
+window.Validator = require('./vendors/validator');
+window._ = require('./vendors/underscore-min');
+window.fetch = require('./vendors/window').fetch;
+window.FormData = require('./vendors/window').FormData;
+window.urldecode =require('./vendors/phpjs').urldecode;
+window.stripslashes = require('./vendors/phpjs').stripslashes;
+window.htmlspecialchars = require('./vendors/phpjs').htmlspecialchars;
+window.in_array = require('./vendors/phpjs').in_array;
+window.storage = require('./vendors/node-localstorage').localStorage;
+window.openpgp = require('./vendors/openpgp');
+window.XRegExp = require('./vendors/xregexp').XRegExp;
+window.jsSHA = require('./vendors/sha');
+window.randomBytes = require('./vendors/crypto').randomBytes;
+window.jsonQ = require('./vendors/jsonQ').jsonQ;

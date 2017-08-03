cdda4ddc0a688987fa0c3058143605fa444aad47
Cédric Alfonsi <cedric@passbolt.com>
2017-08-03 12:17:43 +0200

    PASSBOLT-2254 Remove logging system from ff legacy entry point

--- src/firefox_legacy/index.js
+++ src/firefox_legacy/index.js
@@ -1,11 +1,9 @@
 // SDK add-on
 const webExtension = require("sdk/webextension");
-var Log = require('../model/log').Log;
 
 webExtension.startup().then(function(api) {
   const {browser} = api;
 
-  Log.write({level: 'debug', message: 'SDK Extension started'});
   browser.runtime.onConnect.addListener(function(port) {
       port.postMessage({
       content: "content from legacy add-on"

ef47d60573692355d94c2bd7c31222060d95e0c2
Priyanga V <priyanga@passbolt.com>
2019-06-11 14:33:04 +0530

    Issue Fix for #70, #71.
    Autofill for self-hosted GitLab instance does not work #70
    Autofill for openstack horizon does not work #71

--- src/all/data/js/quickaccess/quickaccess.js
+++ src/all/data/js/quickaccess/quickaccess.js
@@ -8,53 +8,53 @@
 var passbolt = passbolt || {};
 passbolt.quickaccess = passbolt.quickaccess || {};
 
-$(function () {
+$(function() {
 
-  passbolt.quickaccess.bootstrap = function () {
-    passbolt.message.on('passbolt.quickaccess.fill-form', fillForm);
-  };
+    passbolt.quickaccess.bootstrap = function() {
+        passbolt.message.on('passbolt.quickaccess.fill-form', fillForm);
+    };
 
-  const fillForm = function (requestId, username, secret) {
-    try {
-      const passwordElement = getPasswordElement();
-      const usernameElement = getUsernameElement(passwordElement);
-      if (usernameElement) {
-        fireEvent.change(usernameElement, { target: { value: username } });
-      }
-      fireEvent.change(passwordElement, { target: { value: secret } });
-      passbolt.message.emit(requestId, 'SUCCESS');
-    } catch (error) {
-      console.error(error);
-      passbolt.message.emit(requestId, 'ERROR', { name: "Error", message: error.message });
-    }
-  };
+    const fillForm = function(requestId, username, secret) {
+        try {
+            const passwordElement = getPasswordElement();
+            const usernameElement = getUsernameElement(passwordElement);
+            if (usernameElement) {
+                fireEvent.change(usernameElement, { target: { value: username } });
+            }
+            fireEvent.change(passwordElement, { target: { value: secret } });
+            passbolt.message.emit(requestId, 'SUCCESS');
+        } catch (error) {
+            console.error(error);
+            passbolt.message.emit(requestId, 'ERROR', { name: "Error", message: error.message });
+        }
+    };
 
-  const getPasswordElement = function () {
-    const passwordElement = document.querySelector("input[type='password']:not([disabled])");
-    if (!passwordElement) {
-      throw new Error('No password element found on the page');
-    }
+    const getPasswordElement = function() {
+        const passwordElement = $(document).find('input').filter(":input[type='password']:visible:enabled")[0];
+        if (!passwordElement) {
+            throw new Error('No password element found on the page');
+        }
 
-    return passwordElement;
-  }
-
-  const getUsernameElement = function (element) {
-    const parentElement = element.parentElement;
-    if (!parentElement) {
-      return false;
+        return passwordElement;
     }
 
-    let usernameElement = parentElement.querySelector("input[type='email']:not([disabled])");
-    if (!usernameElement) {
-      usernameElement = parentElement.querySelector("input[type='text']:not([disabled])");
-    }
+    const getUsernameElement = function(element) {
+        const parentElement = element.parentElement;
+        if (!parentElement) {
+            return false;
+        }
 
-    if (!usernameElement) {
-      return getUsernameElement(parentElement);
-    }
+        let usernameElement = $(parentElement).find('input').filter(":input[type='email']:visible:enabled")[0];
+        if (!usernameElement) {
+            usernameElement = $(parentElement).find('input').filter(":input[type='text']:visible:enabled")[0];
+        }
 
-    return usernameElement;
-  };
+        if (!usernameElement) {
+            return getUsernameElement(parentElement);
+        }
+
+        return usernameElement;
+    };
 
 });
 undefined; // result must be structured-clonable data

221cbfb6b945db9cefad51ecdb3af32861e0d39b
Remy Bertot <remy@passbolt.com>
2020-05-04 12:35:59 +0200

    Rename userAbortsOperationError part 2

---
+++ src/all/background_page/error/userAbortsOperationError.js
@@ -0,0 +1,15 @@
+/**
+ * Users aborts operation error.
+ *
+ * @copyright (c) 2019 Passbolt SA
+ * @licence GNU Affero General Public License http://www.gnu.org/licenses/agpl-3.0.en.html
+ */
+
+class UserAbortsOperationError extends Error {
+  constructor(message){
+    super(message);
+    this.name = 'UserAbortsOperationError';
+  }
+}
+
+exports.UserAbortsOperationError = UserAbortsOperationError;

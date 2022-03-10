c3b2eec592a4affd85db0ea6f4a2c0cb98793a03
Stéphane Loegel <stephane@passbolt.com>
2022-03-10 10:28:04 +0000

    PB-13921 - Follow-up: Rollback code architecture attempt with the usage of...

--- src/all/background_page/controller/accountRecovery/accountRecoverySaveOrganizationPolicyController.js
+++ src/all/background_page/controller/accountRecovery/accountRecoverySaveOrganizationPolicyController.js
@@ -15,10 +15,14 @@ const {i18n} = require('../../sdk/i18n');
 const {AccountRecoveryModel} = require("../../model/accountRecovery/accountRecoveryModel");
 const {AccountRecoveryOrganizationPolicyEntity} = require("../../model/entity/accountRecovery/accountRecoveryOrganizationPolicyEntity");
 const {PrivateGpgkeyEntity} = require("../../model/entity/gpgkey/privateGpgkeyEntity");
-const PassphraseController = require("../../controller/passphrase/passphraseController");
-const {ProgressService} = require("../../service/progress/progressService");
+const {AccountRecoveryPrivateKeyPasswordsCollection} = require("../../model/entity/accountRecovery/accountRecoveryPrivateKeyPasswordsCollection");
 const {Keyring} = require("../../model/keyring");
-const {SaveAccountRecoveryOrganizationSettingsScenario} = require('../../scenarios/accountRecovery/saveAccountRecoveryOrganizationSettingsScenario');
+const {ProgressService} = require("../../service/progress/progressService");
+const {DecryptPrivateKeyService} = require('../../service/crypto/decryptPrivateKeyService');
+const {ReEncryptMessageService} = require("../../service/crypto/reEncryptMessageService");
+const {SignGpgKeyService} = require('../../service/crypto/signGpgKeyService');
+const {RevokeGpgKeyService} = require('../../service/crypto/revokeGpgKeyService');
+const PassphraseController = require("../../controller/passphrase/passphraseController");
 
 /**
  * Controller related to the account recovery save settings
@@ -35,8 +39,7 @@ class AccountRecoverySaveOrganizationPolicyController {
     this.requestId = requestId;
     const accountRecoveryModel = new AccountRecoveryModel(apiClientOptions);
     this.accountRecoveryModel = accountRecoveryModel;
-    const progressService = new ProgressService(this.worker, i18n.t("Rekeying users' key"));
-    this.saveAccountRecoveryOrganizationSettingsScenario = new SaveAccountRecoveryOrganizationSettingsScenario(progressService, accountRecoveryModel);
+    this.progressService = new ProgressService(this.worker, i18n.t("Rekeying users' key"));
   }
 
   /**
@@ -64,19 +67,116 @@ class AccountRecoverySaveOrganizationPolicyController {
    * @return {Promise<AccountRecoveryOrganizationPolicyEntity>}
    */
   async exec(organizationPolicyDto, organizationPrivateKeyDto = null) {
+    const userPassphrase = await PassphraseController.request(this.worker);
     const newOrganizationPolicy = new AccountRecoveryOrganizationPolicyEntity(organizationPolicyDto);
     const organizationPrivateKey = organizationPrivateKeyDto ? new PrivateGpgkeyEntity(organizationPrivateKeyDto) : null;
 
-    const userPassphrase = await PassphraseController.request(this.worker);
-    const userPrivateKey = new PrivateGpgkeyEntity({
-      armored_key: (new Keyring()).findPrivate().armoredKey,
-      passphrase: userPassphrase
-    });
-
     const currentOrganizationPolicy = await this.accountRecoveryModel.findOrganizationPolicy();
 
-    return this.saveAccountRecoveryOrganizationSettingsScenario
-      .run(newOrganizationPolicy, currentOrganizationPolicy, userPrivateKey, organizationPrivateKey);
+    const hasToRevokeCurrentOrganizationKey = this._hasToRevokedCurrentORK(newOrganizationPolicy, currentOrganizationPolicy);
+    const hasToSignNewOrganizationKey = this._hasToSignNewORK(newOrganizationPolicy, currentOrganizationPolicy);
+    const hasToReKeyPrivateKeyPasswords = hasToRevokeCurrentOrganizationKey && hasToSignNewOrganizationKey;
+
+    const saveOrganizationPolicyDto = newOrganizationPolicy.toDto({account_recovery_organization_public_key: true});
+
+    const signingKeys = [];
+    if (hasToRevokeCurrentOrganizationKey) {
+      const decryptedOrganizationPrivateKey = await DecryptPrivateKeyService.decryptPrivateGpgKeyEntity(organizationPrivateKey);
+      saveOrganizationPolicyDto.account_recovery_organization_revoked_key = {armored_key: await RevokeGpgKeyService.revoke(decryptedOrganizationPrivateKey)};
+
+      signingKeys.push(decryptedOrganizationPrivateKey);
+
+      if (hasToReKeyPrivateKeyPasswords) {
+        const privateKeyPasswordCollection = await this._reEncryptPrivateKeyPasswords(newOrganizationPolicy.armoredKey, decryptedOrganizationPrivateKey);
+        saveOrganizationPolicyDto.account_recovery_private_key_passwords = privateKeyPasswordCollection.toDto();
+      }
+    }
+
+    if (hasToSignNewOrganizationKey) {
+      const usersPrivateKey = (new Keyring()).findPrivate().armoredKey;
+      const decryptedAdministratorKey = await DecryptPrivateKeyService.decrypt(usersPrivateKey, userPassphrase);
+      signingKeys.push(decryptedAdministratorKey);
+
+      const signedNewORK = await SignGpgKeyService.sign(newOrganizationPolicy.armoredKey, signingKeys);
+      saveOrganizationPolicyDto.account_recovery_organization_public_key = {armored_key: signedNewORK.armor()};
+    }
+
+    const saveOrganizationPolicyEntity = new AccountRecoveryOrganizationPolicyEntity(saveOrganizationPolicyDto);
+
+    return this.accountRecoveryModel.saveOrganizationPolicy(saveOrganizationPolicyEntity);
+  }
+
+  /**
+   * Returns true if the current ORK needs to be revoked.
+   * It is the case when the ORK is changed or if the new policy is set to "disabled".
+   *
+   * @param {AccountRecoveryOrganizationPolicyEntity} newPolicyEntity
+   * @param {AccountRecoveryOrganizationPolicyEntity} currentPolicyEntity
+   * @returns {bool}
+   */
+  _hasToRevokedCurrentORK(newPolicyEntity, currentPolicyEntity) {
+    if (currentPolicyEntity.isDisabled) {
+      return false;
+    }
+
+    const newPolicyIsDisabled = newPolicyEntity.isDisabled;
+    const keyHasChanged = newPolicyEntity.armoredKey !== currentPolicyEntity.armoredKey;
+
+    return keyHasChanged || newPolicyIsDisabled;
+  }
+
+  /**
+   * Returns true if the new ORK needs to be signed.
+   * It needs to be signed if the new ORK is enabled and changed from the current one.
+   *
+   * @param {AccountRecoveryOrganizationPolicyEntity} newPolicyEntity
+   * @param {AccountRecoveryOrganizationPolicyEntity} currentPolicyEntity
+   * @returns {bool}
+   */
+  _hasToSignNewORK(newPolicyEntity, currentPolicyEntity) {
+    if (newPolicyEntity.isDisabled) {
+      return false;
+    }
+
+    if (currentPolicyEntity.isDisabled) {
+      return true;
+    }
+
+    const oldPolicyDisabled = currentPolicyEntity.isDisabled;
+    const keyHasChanged = currentPolicyEntity.armoredKey !== newPolicyEntity.armoredKey;
+
+    return oldPolicyDisabled || keyHasChanged;
+  }
+
+  /**
+   * Reencrypt the existing account recovery private key passwords with the new organization recovery key.
+   *
+   * @param {string} encryptionKey
+   * @param {string} decryptionKey
+   * @returns {Promise<AccountRecoveryPrivateKeyPasswordsCollection>}
+   */
+  async _reEncryptPrivateKeyPasswords(encryptionKey, decryptionKey) {
+    const accountRecoveryPrivateKeyPasswords = await this.accountRecoveryModel.findAccountRecoveryPrivateKeyPasswords();
+    if (accountRecoveryPrivateKeyPasswords.length === 0) {
+      return new AccountRecoveryPrivateKeyPasswordsCollection();
+    }
+
+    await this.progressService.start(accountRecoveryPrivateKeyPasswords.length, i18n.t("Updating users' key..."));
+
+    const newAccountRecoveryPrivateKeyPasswords = [];
+    const items = accountRecoveryPrivateKeyPasswords.items;
+    for (let i = 0; i < items.length; i++) {
+      const encryptedKeyData = await ReEncryptMessageService.reEncrypt(items[i].data, encryptionKey, decryptionKey, decryptionKey, decryptionKey);
+      const privateKeyPasswordDto = {
+        ...items[i].toDto(),
+        data: encryptedKeyData
+      };
+      newAccountRecoveryPrivateKeyPasswords.push(privateKeyPasswordDto);
+      await this.progressService.finishStep();
+    }
+
+    this.progressService.close();
+    return new AccountRecoveryPrivateKeyPasswordsCollection(newAccountRecoveryPrivateKeyPasswords);
   }
 }
 

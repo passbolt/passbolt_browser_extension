136ff27734c3163d82497b72025413668b1c58f4
Cedric Alfonsi <cedric@passbolt.com>
2022-03-28 21:58:23 +0200

    PB-14274 - Review account recovery request workflow should retrieve the account recovery request itself

---
+++ src/all/background_page/controller/accountRecovery/accountRecoveryGetRequestController.js
@@ -0,0 +1,68 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) 2022 Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) 2022 Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         3.6.0
+ */
+
+const {AccountRecoveryModel} = require("../../model/accountRecovery/accountRecoveryModel");
+
+class AccountRecoveryGetRequestController {
+  /**
+   * AccountRecoveryGetRequestController constructor
+   * @param {Worker} worker
+   * @param {string} requestId uuid
+   * @param {ApiClientOptions} apiClientOptions
+   */
+  constructor(worker, requestId, apiClientOptions) {
+    this.worker = worker;
+    this.requestId = requestId;
+    this.accountRecoveryModel = new AccountRecoveryModel(apiClientOptions);
+  }
+
+  /**
+   * Wrapper of exec function to run it with worker.
+   *
+   * @param {string} accountRecoverRequestId The account recovery request id to retrieve
+   * @return {Promise<void>}
+   */
+  async _exec(accountRecoverRequestId) {
+    try {
+      const accountRecoveryRequest = await this.exec(accountRecoverRequestId);
+      this.worker.port.emit(this.requestId, "SUCCESS", accountRecoveryRequest);
+    } catch (error) {
+      console.error(error);
+      this.worker.port.emit(this.requestId, "ERROR", error);
+    }
+  }
+
+  /**
+   * Get an account recovery request.
+   *
+   * @param {string} accountRecoverRequestId The account recovery request id to retrieve
+   * @return {Promise<AccountRecoveryRequestEntity|null>}
+   */
+  async exec(accountRecoverRequestId) {
+    if (typeof accountRecoverRequestId === "undefined") {
+      throw new Error("An account recovery request id is required.");
+    }
+    if (typeof accountRecoverRequestId !== "string") {
+      throw new Error("The account recovery request should be a string.");
+    }
+    if (!Validator.isUUID(accountRecoverRequestId)) {
+      throw new Error("The account recovery request should be a valid uuid.");
+    }
+
+    const contains = {"creator": true, "creator.gpgkey": true};
+    return this.accountRecoveryModel.findRequestById(accountRecoverRequestId, contains);
+  }
+}
+
+exports.AccountRecoveryGetRequestController = AccountRecoveryGetRequestController;
---
+++ src/all/background_page/controller/accountRecovery/accountRecoveryGetRequestController.test.js
@@ -0,0 +1,58 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) 2022 Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) 2022 Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         3.6.0
+ */
+
+import {v4 as uuidv4} from "uuid";
+import {enableFetchMocks} from "jest-fetch-mock";
+import {mockApiResponse} from "../../../tests/mocks/mockApiResponse";
+import {defaultApiClientOptions} from "../../service/api/apiClient/apiClientOptions.test.data";
+import {defaultAccountRecoveryRequestDto} from "../../model/entity/accountRecovery/accountRecoveryRequestEntity.test.data";
+import {AccountRecoveryGetRequestController} from "./accountRecoveryGetRequestController";
+import {AccountRecoveryRequestEntity} from "../../model/entity/accountRecovery/accountRecoveryRequestEntity";
+
+beforeEach(() => {
+  enableFetchMocks();
+});
+
+describe("AccountRecoveryGetRequestController", () => {
+  describe("AccountRecoveryGetRequestController::exec", () => {
+    it("Should retrieve an account recovery request.", async() => {
+      // Mock API fetch account recovery organization policy response.
+      const mockApiResult = defaultAccountRecoveryRequestDto();
+      fetch.doMock(() => mockApiResponse(mockApiResult));
+
+      const controller = new AccountRecoveryGetRequestController(null, null, defaultApiClientOptions());
+      const accountRecoveryRequest = await controller.exec(uuidv4());
+
+      expect.assertions(1);
+      const accountRecoveryRequestDto = accountRecoveryRequest.toDto(AccountRecoveryRequestEntity.ALL_CONTAIN_OPTIONS);
+      await expect(accountRecoveryRequestDto).toEqual(mockApiResult);
+    });
+
+    it("Should throw an error if the account recovery request id is not a valid.", async() => {
+      // Mock API fetch account recovery organization policy response.
+      const mockApiResult = defaultAccountRecoveryRequestDto();
+      fetch.doMock(() => mockApiResponse(mockApiResult));
+
+      const controller = new AccountRecoveryGetRequestController(null, null, defaultApiClientOptions());
+
+      expect.assertions(3);
+      const promiseMissingParameter = controller.exec();
+      await expect(promiseMissingParameter).rejects.toThrowError("An account recovery request id is required");
+      const promiseInvalidTypeParameter = controller.exec(2);
+      await expect(promiseInvalidTypeParameter).rejects.toThrowError("The account recovery request should be a string.");
+      const promiseNotValidUuid = controller.exec("not-a-valid-uuid");
+      await expect(promiseNotValidUuid).rejects.toThrowError("The account recovery request should be a valid uuid.");
+    });
+  });
+});

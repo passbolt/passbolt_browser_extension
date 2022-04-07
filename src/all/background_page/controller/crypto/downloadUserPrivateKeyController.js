cb6866c027a5a9a81c78e204ddb8195367897979
Stéphane Loegel <stephane@passbolt.com>
2022-04-07 07:58:08 +0000

    PB-14056 - Follow-up: migrate download user's private key to a controller...

---
+++ src/all/background_page/controller/crypto/downloadUserPrivateKeyController.js
@@ -0,0 +1,69 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) 2022 Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) 2022 Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         3.6.0
+ */
+
+const {i18n} = require('../../sdk/i18n');
+const {GpgKeyError} = require('../../error/GpgKeyError');
+const {Keyring} = require('../../model/keyring');
+const fileController = require('../fileController');
+const PassphraseController = require('../passphrase/passphraseController');
+
+const PRIVATE_KEY_FILENAME = "passbolt_private.asc";
+const MIME_TYPE_TEXT_PLAIN = "text/plain";
+
+class DownloadUserPrivateKeyController {
+  /**
+   * CheckPassphraseController constructor
+   * @param {Worker} worker
+   * @param {string} requestId uuid
+   */
+  constructor(worker, requestId) {
+    this.worker = worker;
+    this.requestId = requestId;
+    this.keyring = new Keyring();
+  }
+
+  /**
+   * Wrapper of exec function to run it with worker.
+   *
+   * @returns {Promise<void>}
+   */
+  async _exec() {
+    try {
+      await this.exec();
+      this.worker.port.emit(this.requestId, "SUCCESS");
+    } catch (error) {
+      console.error(error);
+      this.worker.port.emit(this.requestId, 'ERROR', error);
+    }
+  }
+
+  /**
+   * Offer to users to download their private key
+   *
+   * @returns {Promise<void>}
+   */
+  async exec() {
+    await PassphraseController.request(this.worker);
+    let privateKey;
+    try {
+      privateKey = this.keyring.findPrivate().armoredKey;
+    } catch (e) {
+      throw new GpgKeyError(i18n.t("Private key not found."));
+    }
+
+    await fileController.saveFile(PRIVATE_KEY_FILENAME, privateKey, MIME_TYPE_TEXT_PLAIN, this.worker.tab.id);
+  }
+}
+
+exports.DownloadUserPrivateKeyController = DownloadUserPrivateKeyController;
---
+++ src/all/background_page/controller/crypto/downloadUserPrivateKeyController.test.js
@@ -0,0 +1,70 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) 2022 Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) 2022 Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         3.6.0
+ */
+
+import {DownloadUserPrivateKeyController} from "./downloadUserPrivateKeyController";
+import {GetGpgKeyInfoService} from "../../service/crypto/getGpgKeyInfoService";
+import {GpgKeyError} from "../../error/GpgKeyError";
+import {MockExtension} from "../../../tests/mocks/mockExtension";
+import {pgpKeys} from "../../../tests/fixtures/pgpKeys/keys";
+import PassphraseController from "../passphrase/passphraseController";
+
+const mockedSaveFile = jest.fn();
+jest.mock('../fileController', () => ({
+  saveFile: jest.fn((fileName, fileContent, fileContentType, workerTabId) => mockedSaveFile(fileName, fileContent, fileContentType, workerTabId))
+}));
+jest.mock("../passphrase/passphraseController");
+
+const expectedTabId = "tabIdentifier";
+const mockedWorker = {tab: {id: expectedTabId}};
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe("DownloadUserPrivateKeyController", () => {
+  it(`Should trigegr a file download with the private key`, async() => {
+    expect.assertions(8);
+    await MockExtension.withConfiguredAccount();
+    PassphraseController.request.mockResolvedValue("");
+    const controller = new DownloadUserPrivateKeyController(mockedWorker, null);
+    const privateKey = pgpKeys.ada;
+
+    mockedSaveFile.mockImplementation(async(fileName, fileContent, fileContentType, workerTabId) => {
+      expect(fileName).toBe("passbolt_private.asc");
+      expect(fileContentType).toBe("text/plain");
+      expect(workerTabId).toBe(expectedTabId);
+
+      const downloadedKeyInfo = await GetGpgKeyInfoService.getKeyInfo(fileContent);
+      expect(downloadedKeyInfo.private).toBe(true);
+      expect(downloadedKeyInfo.keyId).toBe(privateKey.key_id);
+      expect(downloadedKeyInfo.fingerprint).toBe(privateKey.fingerprint);
+    });
+
+    await controller.exec();
+
+    expect(mockedSaveFile).toHaveBeenCalledTimes(1);
+    expect(PassphraseController.request).toHaveBeenCalledTimes(1);
+  });
+
+  it(`Should throw an exception if the user's private key can't be find`, async() => {
+    expect.assertions(3);
+    MockExtension.withMissingPrivateKeyAccount();
+    PassphraseController.request.mockResolvedValue("");
+    const controller = new DownloadUserPrivateKeyController(mockedWorker, null);
+
+    await expect(controller.exec()).rejects.toThrowError(new GpgKeyError("Private key not found."));
+    expect(mockedSaveFile).not.toHaveBeenCalled();
+    expect(PassphraseController.request).toHaveBeenCalledTimes(1);
+  });
+});

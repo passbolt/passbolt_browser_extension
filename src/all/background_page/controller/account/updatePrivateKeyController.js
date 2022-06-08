d0e7e3834cc3791e9e69915bb28dae5adbadd98c
Stéphane Loegel <stephane@passbolt.com>
2022-06-08 11:27:29 +0200

    PB-16763 - Fix as LU, I should be able to change my passphrase and download the new recovery kit

---
+++ src/all/background_page/controller/account/updatePrivateKeyController.js
@@ -0,0 +1,64 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) 2022 Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) 2022 Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         3.6.3
+ */
+const {AccountModel} = require("../../model/account/accountModel");
+const {User} = require("../../model/user");
+const fileController = require('../fileController');
+
+const RECOVERY_KIT_FILENAME = "passbolt-recovery-kit.asc";
+
+class UpdatePrivateKeyController {
+  /**
+   * UpdatePrivateKeyController constructor
+   * @param {Worker} worker
+   * @param {string} requestId uuid
+   * @param {ApiClientOptions} apiClientOptions the api client options
+   */
+  constructor(worker, requestId, apiClientOptions) {
+    this.worker = worker;
+    this.requestId = requestId;
+    this.apiClientOptions = apiClientOptions;
+    this.accountModel = new AccountModel(apiClientOptions);
+  }
+
+  /**
+   * Wrapper of exec function to run it with worker.
+   *
+   * @return {Promise<void>}
+   */
+  async _exec(oldPassphrase, newPassphrase) {
+    try {
+      await this.exec(oldPassphrase, newPassphrase);
+      this.worker.port.emit(this.requestId, "SUCCESS");
+    } catch (error) {
+      console.error(error);
+      this.worker.port.emit(this.requestId, 'ERROR', error);
+    }
+  }
+
+  /**
+   * Updates the passphrase of the current user's private key and then starts a download of the new key.
+   *
+   * @returns {Promise<void>}
+   */
+  async exec(oldPassphrase, newPassphrase) {
+    if (typeof oldPassphrase !== 'string' || typeof newPassphrase !== 'string') {
+      throw new Error('The old and new passphrase have to be string');
+    }
+    const userPrivateArmoredKey = await this.accountModel.updatePrivateKey(oldPassphrase, newPassphrase);
+    await User.getInstance().flushMasterPassword();
+    await fileController.saveFile(RECOVERY_KIT_FILENAME, userPrivateArmoredKey, "text/plain", this.worker.tab.id);
+  }
+}
+
+exports.UpdatePrivateKeyController = UpdatePrivateKeyController;
---
+++ src/all/background_page/controller/account/updatePrivateKeyController.test.js
@@ -0,0 +1,99 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) 2022 Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) 2022 Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         3.6.3
+ */
+import {v4 as uuidv4} from "uuid";
+import {pgpKeys} from "../../../../../test/fixtures/pgpKeys/keys";
+import {MockExtension} from "../../../../../test/mocks/mockExtension";
+import {defaultApiClientOptions} from "../../service/api/apiClient/apiClientOptions.test.data";
+import {DecryptPrivateKeyService} from "../../service/crypto/decryptPrivateKeyService";
+import {readKeyOrFail, assertEncryptedPrivateKey} from "../../utils/openpgp/openpgpAssertions";
+import {UpdatePrivateKeyController} from "./updatePrivateKeyController";
+
+const mockedSaveFile = jest.fn();
+jest.mock('../fileController', () => ({
+  saveFile: jest.fn((fileName, fileContent, fileContentType, workerTabId) => mockedSaveFile(fileName, fileContent, fileContentType, workerTabId))
+}));
+
+describe("UpdatePrivateKeyController", () => {
+  describe("UpdatePrivateKeyController::exec", () => {
+    it("Should trigger the download of the recovery kit with the new passphrase.", async() => {
+      expect.assertions(4);
+
+      await MockExtension.withConfiguredAccount();
+      mockedSaveFile.mockImplementation(async(fileName, fileContent, fileContentType, workerTabId) => {
+        expect(fileName).toStrictEqual("passbolt-recovery-kit.asc");
+        expect(fileContentType).toStrictEqual("text/plain");
+        expect(workerTabId).toStrictEqual(worker.tab.id);
+
+        const key = await readKeyOrFail(fileContent);
+        assertEncryptedPrivateKey(key);
+
+        const decryptedPrivateKey = await DecryptPrivateKeyService.decrypt(key, newPassphrase);
+        expect(decryptedPrivateKey).toBeTruthy();
+      });
+
+      const worker = {
+        tab: {
+          id: uuidv4()
+        }
+      };
+
+      const controller = new UpdatePrivateKeyController(worker, null, defaultApiClientOptions());
+      const oldPassphrase = pgpKeys.ada.passphrase;
+      const newPassphrase = "newPassphrase";
+      await controller.exec(oldPassphrase, newPassphrase);
+    });
+
+    it("Should throw an error if no passphrase is provided.", async() => {
+      expect.assertions(2);
+      await MockExtension.withConfiguredAccount();
+      const controller = new UpdatePrivateKeyController(null, null, defaultApiClientOptions());
+
+      const nullPassphrase = null;
+      const stringPassphrase = "stringPassphrase";
+      const expectedError = new Error("The old and new passphrase have to be string");
+      try {
+        await controller.exec(nullPassphrase, stringPassphrase);
+      } catch (e) {
+        expect(e).toStrictEqual(expectedError);
+      }
+
+      try {
+        await controller.exec(stringPassphrase, nullPassphrase);
+      } catch (e) {
+        expect(e).toStrictEqual(expectedError);
+      }
+    });
+
+    it("Should throw an error if passphrases are not strings.", async() => {
+      expect.assertions(2);
+      await MockExtension.withConfiguredAccount();
+      const controller = new UpdatePrivateKeyController(null, null, defaultApiClientOptions());
+
+      const notStringPassphrase = {};
+      const stringPassphrase = "stringPassphrase";
+      const expectedError = new Error("The old and new passphrase have to be string");
+      try {
+        await controller.exec(notStringPassphrase, stringPassphrase);
+      } catch (e) {
+        expect(e).toStrictEqual(expectedError);
+      }
+
+      try {
+        await controller.exec(stringPassphrase, notStringPassphrase);
+      } catch (e) {
+        expect(e).toStrictEqual(expectedError);
+      }
+    });
+  });
+});

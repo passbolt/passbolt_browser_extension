251cd7d6fee95f20a8f7665f4414b1d08512d7d7
Stéphane Loegel <stephane@passbolt.com>
2023-01-18 23:39:17 +0100

    PB-21813 - Unit test the private key's passphrase rotation SSO kit regeneration

--- src/all/background_page/controller/account/updatePrivateKeyController.test.js
+++ src/all/background_page/controller/account/updatePrivateKeyController.test.js
@@ -11,6 +11,8 @@
  * @link          https://www.passbolt.com Passbolt(tm)
  * @since         3.6.3
  */
+import "../../../../../test/mocks/mockSsoDataStorage";
+import "../../../../../test/mocks/mockCryptoKey";
 import {v4 as uuidv4} from "uuid";
 import {pgpKeys} from "../../../../../test/fixtures/pgpKeys/keys";
 import MockExtension from "../../../../../test/mocks/mockExtension";
@@ -19,15 +21,40 @@ import DecryptPrivateKeyService from "../../service/crypto/decryptPrivateKeyServ
 import UpdatePrivateKeyController from "./updatePrivateKeyController";
 import {OpenpgpAssertion} from "../../utils/openpgp/openpgpAssertions";
 import FileService from "../../service/file/fileService";
+import SsoDataStorage from "../../service/indexedDB_storage/ssoDataStorage";
+import {clientSsoKit} from "../../model/entity/sso/ssoKitClientPart.test.data";
+import GenerateSsoKitService from "../../service/sso/generateSsoKitService";
+import {anonymousOrganizationSettings} from "../../model/entity/organizationSettings/organizationSettingsEntity.test.data";
+import {mockApiResponse, mockApiResponseError} from "../../../../../test/mocks/mockApiResponse";
+import {enableFetchMocks} from "jest-fetch-mock";
+import SsoKitClientPartEntity from "../../model/entity/sso/ssoKitClientPartEntity";
+import SsoKitServerPartEntity from "../../model/entity/sso/ssoKitServerPartEntity";
+import PassphraseStorageService from "../../service/session_storage/passphraseStorageService";
+import InvalidMasterPasswordError from "../../error/invalidMasterPasswordError";
 
 const mockedSaveFile = jest.spyOn(FileService, "saveFile");
 
+beforeEach(() => {
+  enableFetchMocks();
+  jest.clearAllMocks();
+  fetch.resetMocks();
+});
+
 describe("UpdatePrivateKeyController", () => {
+  const mockOrganisationSettingCall = (ssoEnabled = false) => {
+    const organizationSettings = anonymousOrganizationSettings();
+    if (ssoEnabled) {
+      organizationSettings.passbolt.plugins.sso = {enabled: true};
+    }
+    fetch.doMockOnceIf(new RegExp('/settings.json'), () => mockApiResponse(organizationSettings, {servertime: Date.now() / 1000}));
+  };
+
   describe("UpdatePrivateKeyController::exec", () => {
     it("Should trigger the download of the recovery kit with the new passphrase.", async() => {
       expect.assertions(4);
 
       await MockExtension.withConfiguredAccount();
+      mockOrganisationSettingCall();
       mockedSaveFile.mockImplementation(async(fileName, fileContent, fileContentType, workerTabId) => {
         expect(fileName).toStrictEqual("passbolt-recovery-kit.asc");
         expect(fileContentType).toStrictEqual("text/plain");
@@ -92,6 +119,195 @@ describe("UpdatePrivateKeyController", () => {
       } catch (e) {
         expect(e).toStrictEqual(expectedError);
       }
+    });
+
+    it("Should update the local SSO kit if one already exists.", async() => {
+      expect.assertions(2);
+      const newPassphrase = "newPassphrase";
+      const ssoKit = clientSsoKit();
+      const newSsoKitId = uuidv4();
+      const newSsoKit = new SsoKitClientPartEntity(clientSsoKit({id: newSsoKitId}));
+      const worker = {
+        tab: {
+          id: uuidv4()
+        }
+      };
+
+      await MockExtension.withConfiguredAccount();
+      SsoDataStorage.setMockedData(ssoKit);
+
+      expect.assertions(3);
+
+      mockOrganisationSettingCall(true);
+      fetch.doMockOnceIf(new RegExp(`/sso/keys/${ssoKit.id}.json`), () => mockApiResponse({}));
+      fetch.doMockOnceIf(new RegExp('/sso/keys.json'), () => mockApiResponse({id: newSsoKitId, data: 'test'}));
+
+      jest.spyOn(GenerateSsoKitService, "generateSsoKits").mockImplementation((passphrase, providerId) => {
+        expect(passphrase).toBe(newPassphrase);
+        expect(providerId).toBe(ssoKit.provider);
+
+        return {
+          serverPart: new SsoKitServerPartEntity({data: 'test'}),
+          clientPart: newSsoKit,
+        };
+      });
+
+      SsoDataStorage.save.mockImplementation(kit => {
+        expect(kit).toBe(newSsoKit);
+      });
+
+      mockedSaveFile.mockImplementation(async() => {});
+
+      const controller = new UpdatePrivateKeyController(worker, null, defaultApiClientOptions());
+      const oldPassphrase = pgpKeys.ada.passphrase;
+      await controller.exec(oldPassphrase, newPassphrase);
+    });
+
+    it("Should not create a local SSO kit if none exists.", async() => {
+      expect.assertions(2);
+      const worker = {
+        tab: {
+          id: uuidv4()
+        }
+      };
+
+      await MockExtension.withConfiguredAccount();
+      SsoDataStorage.setMockedData(null);
+
+      expect.assertions(2);
+      mockOrganisationSettingCall(true);
+
+      const shouldNotHaveBeenCalledError = new Error("This API request should not have been made");
+      fetch.doMockOnceIf(new RegExp(`/sso/keys/[a-fA-F0-9-].*\.json`), () => { throw shouldNotHaveBeenCalledError; });
+      fetch.doMockOnceIf(new RegExp('/sso/keys.json'), () => { throw shouldNotHaveBeenCalledError; });
+
+      jest.spyOn(GenerateSsoKitService, "generateSsoKits");
+
+      mockedSaveFile.mockImplementation(() => {});
+
+      const controller = new UpdatePrivateKeyController(worker, null, defaultApiClientOptions());
+      await controller.exec(pgpKeys.ada.passphrase, "newPassphrase");
+
+      expect(GenerateSsoKitService.generateSsoKits).not.toHaveBeenCalled();
+      expect(SsoDataStorage.save).not.toHaveBeenCalled();
+    });
+
+    it("Should not update the passsphrase if the kit can't be send to the server.", async() => {
+      mockOrganisationSettingCall(true);
+      const newPassphrase = "newPassphrase";
+      const ssoKit = clientSsoKit();
+      const newSsoKitId = uuidv4();
+      const newSsoKit = new SsoKitClientPartEntity(clientSsoKit({id: newSsoKitId}));
+      const worker = {
+        tab: {
+          id: uuidv4()
+        }
+      };
+
+      await MockExtension.withConfiguredAccount();
+      SsoDataStorage.setMockedData(ssoKit);
+
+      expect.assertions(3);
+      fetch.doMockOnceIf(new RegExp(`/sso/keys/${ssoKit.id}.json`), () => mockApiResponse({}));
+
+      const expectedError = new Error("Something went wrong");
+      fetch.doMockOnceIf(new RegExp('/sso/keys.json'), () => { throw expectedError; });
+
+      jest.spyOn(GenerateSsoKitService, "generateSsoKits").mockImplementation(() => ({
+        serverPart: new SsoKitServerPartEntity({data: 'test'}),
+        clientPart: newSsoKit,
+      }));
+      jest.spyOn(PassphraseStorageService, "flushPassphrase");
+
+      mockedSaveFile.mockImplementation(async() => {});
+
+      const controller = new UpdatePrivateKeyController(worker, null, defaultApiClientOptions());
+      const oldPassphrase = pgpKeys.ada.passphrase;
+      try {
+        await controller.exec(oldPassphrase, newPassphrase);
+      } catch (_) { }
+
+      expect(SsoDataStorage.save).not.toHaveBeenCalled();
+      expect(PassphraseStorageService.flushPassphrase).not.toHaveBeenCalled();
+      expect(mockedSaveFile).not.toHaveBeenCalled();
+    });
+
+    it("Should ignore the sso kit deletion on server side if it gets a 404.", async() => {
+      const newPassphrase = "newPassphrase";
+      const ssoKit = clientSsoKit();
+      const newSsoKitId = uuidv4();
+      const newSsoKit = new SsoKitClientPartEntity(clientSsoKit({id: newSsoKitId}));
+      const worker = {
+        tab: {
+          id: uuidv4()
+        }
+      };
+
+      await MockExtension.withConfiguredAccount();
+      SsoDataStorage.setMockedData(ssoKit);
+
+      expect.assertions(1);
+
+      mockOrganisationSettingCall(true);
+      fetch.doMockOnceIf(new RegExp(`/sso/keys/${ssoKit.id}.json`), () => mockApiResponseError(404));
+      fetch.doMockOnceIf(new RegExp('/sso/keys.json'), () => mockApiResponse({id: newSsoKitId, data: 'test'}));
+
+      jest.spyOn(GenerateSsoKitService, "generateSsoKits").mockImplementation(() => ({
+        serverPart: new SsoKitServerPartEntity({data: 'test'}),
+        clientPart: newSsoKit,
+      }));
+
+      SsoDataStorage.save.mockImplementation(kit => {
+        expect(kit).toBe(newSsoKit);
+      });
+
+      mockedSaveFile.mockImplementation(async() => {});
+
+      const controller = new UpdatePrivateKeyController(worker, null, defaultApiClientOptions());
+      const oldPassphrase = pgpKeys.ada.passphrase;
+      await controller.exec(oldPassphrase, newPassphrase);
+    });
+
+    it("Should not generate another SSO kit if the passphrase can't be rotated.", async() => {
+      const newPassphrase = "newPassphrase";
+      const ssoKit = clientSsoKit();
+      const newSsoKitId = uuidv4();
+      const newSsoKit = new SsoKitClientPartEntity(clientSsoKit({id: newSsoKitId}));
+      const worker = {
+        tab: {
+          id: uuidv4()
+        }
+      };
+
+      await MockExtension.withConfiguredAccount();
+      SsoDataStorage.setMockedData(ssoKit);
+
+      expect.assertions(4);
+
+      mockOrganisationSettingCall(true);
+
+      const shouldNotHaveBeenCalledError = new Error("This API request should not have been made");
+      fetch.doMockOnceIf(new RegExp(`/sso/keys/[a-fA-F0-9-].*\.json`), () => { throw shouldNotHaveBeenCalledError; });
+      fetch.doMockOnceIf(new RegExp('/sso/keys.json'), () => { throw shouldNotHaveBeenCalledError; });
+
+      jest.spyOn(GenerateSsoKitService, "generateSsoKits");
+
+      SsoDataStorage.save.mockImplementation(kit => {
+        expect(kit).toBe(newSsoKit);
+      });
+
+      mockedSaveFile.mockImplementation(async() => {});
+
+      const controller = new UpdatePrivateKeyController(worker, null, defaultApiClientOptions());
+      try {
+        await controller.exec("wrong passphrase", newPassphrase);
+      } catch (e) {
+        expect(e).toBeInstanceOf(InvalidMasterPasswordError);
+      }
+
+      expect(GenerateSsoKitService.generateSsoKits).not.toHaveBeenCalled();
+      expect(SsoDataStorage.save).not.toHaveBeenCalled();
+      expect(mockedSaveFile).not.toHaveBeenCalled();
     });
   });
 });
--- src/all/background_page/model/entity/sso/ssoKitClientPart.test.data.js
+++ src/all/background_page/model/entity/sso/ssoKitClientPart.test.data.js
@@ -14,7 +14,7 @@
 import {v4 as uuid} from "uuid";
 import GenerateSsoIvService from "../../../service/crypto/generateSsoIvService";
 
-export const clientSsoKit = async(data = {}) => {
+export const clientSsoKit = (data = {}) => {
   const algorithm = {
     name: "AES-GCM",
     length: 256
@@ -24,7 +24,8 @@ export const clientSsoKit = async(data = {}) => {
     id: uuid(),
     secret: Buffer.from(JSON.stringify("Don't tell everybody, this is a secret")).toString('base64'),
     nek: nek,
-    iv1: await GenerateSsoIvService.generateIv(),
-    iv2: await GenerateSsoIvService.generateIv()
+    iv1: GenerateSsoIvService.generateIv(),
+    iv2: GenerateSsoIvService.generateIv(),
+    provider: "azure"
   }, data);
 };

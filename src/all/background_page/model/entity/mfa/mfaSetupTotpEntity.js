0d1a315d29739b88b0116baebb89c838d6f76bc9
Stéphane Loegel <stephane@passbolt.com>
2023-09-28 11:51:32 +0000

    Apply 16 suggestion(s) to 16 file(s)

--- src/all/background_page/model/entity/mfa/mfaSetupTotpEntity.js
+++ src/all/background_page/model/entity/mfa/mfaSetupTotpEntity.js
@@ -9,7 +9,7 @@
  * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
  * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
  * @link          https://www.passbolt.com Passbolt(tm)
- * @since         4.3.0
+ * @since         4.4.0
  */
 
 import Entity from "passbolt-styleguide/src/shared/models/entity/abstract/entity";

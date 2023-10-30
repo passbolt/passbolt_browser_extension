9f82ee4c6030cde3d784c1990654c635ad9713ae
Stéphane Loegel <stephane@passbolt.com>
2023-10-30 10:08:45 +0000

    Feature/pb 28266 as an administration i can enable the password expiry feature

---
+++ src/all/background_page/service/api/passwordExpiry/passwordExpirySettingsService.js
@@ -0,0 +1,72 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         4.4.0
+ */
+import AbstractService from "../abstract/abstractService";
+
+const PASSWORD_EXPIRY_SETTINGS_RESOURCE_NAME = 'password-expiry/settings';
+
+class PasswordExpirySettingsService extends AbstractService {
+  /**
+   * Constructor
+   *
+   * @param {ApiClientOptions} apiClientOptions
+   * @public
+   */
+  constructor(apiClientOptions) {
+    super(apiClientOptions, PasswordExpirySettingsService.RESOURCE_NAME);
+  }
+
+  /**
+   * Get settings for password-expiry.
+   * @returns {Promise<Object>} Response body
+   * @public
+   */
+  async find() {
+    const setting = await this.apiClient.findAll();
+    return setting.body;
+  }
+
+  /**
+   * Create password-expiry settings on the API.
+   * @param {Object} data the settings to save
+   * @returns {Promise<Object>} Response body
+   * @public
+   */
+  async create(data) {
+    this.assertNonEmptyData(data);
+    const setting = await this.apiClient.create(data);
+    return setting.body;
+  }
+
+  /**
+   * Delete the given password-expiry settings.
+   * @param {Object} data the settings to save
+   * @returns {Promise<Object>} Response body
+   * @public
+   */
+  async delete(passwordExpirySettingsId) {
+    await this.apiClient.delete(passwordExpirySettingsId);
+  }
+
+  /**
+   * API Resource Name
+   *
+   * @returns {string}
+   * @public
+   */
+  static get RESOURCE_NAME() {
+    return PASSWORD_EXPIRY_SETTINGS_RESOURCE_NAME;
+  }
+}
+
+export default PasswordExpirySettingsService;

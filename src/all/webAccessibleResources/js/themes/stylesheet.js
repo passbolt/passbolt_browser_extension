1fa1d6a6abe86144d60b29f1accf1fc37ab1ee46
Stéphane Loegel <stephane@passbolt.com>
2024-01-12 16:07:22 +0100

    PB-27805 - Fix the styling issue from stylesheet.js

--- src/all/webAccessibleResources/js/themes/stylesheet.js
+++ src/all/webAccessibleResources/js/themes/stylesheet.js
@@ -32,9 +32,9 @@
 
       this.theme = await this.getTheme();
       const cssFile = cssInfoTag.dataset.file;
-      const baseUrl = window.location.origin;
 
-      this.getLinkTag().setAttribute("href", `${baseUrl}/webAccessibleResources/css/themes/${this.theme}/${cssFile}`);
+      // we went for full URL with schema and domain but, Safari doesn't support that
+      this.getLinkTag().setAttribute("href", `/webAccessibleResources/css/themes/${this.theme}/${cssFile}`);
     }
 
     getLinkTag() {

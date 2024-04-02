b5858550252db15c4e3a45bd0e3f27fcd72186fe
Stéphane Loegel <stephane@passbolt.com>
2024-04-02 17:25:53 +0200

    PB-32420 - Fix double calls to PownedPassword API service

--- src/all/background_page/event/secretEvents.js
+++ src/all/background_page/event/secretEvents.js
@@ -6,7 +6,6 @@
  * @licence GNU Affero General Public License http://www.gnu.org/licenses/agpl-3.0.en.html
  */
 import SecretDecryptController from "../controller/secret/secretDecryptController";
-import PownedPasswordController from '../controller/secret/pownedPasswordController';
 
 /**
  * Listens the secret events
@@ -24,18 +23,6 @@ const listen = function(worker, apiClientOptions, account) {
   worker.port.on('passbolt.secret.decrypt', async(requestId, resourceId) => {
     const controller = new SecretDecryptController(worker, requestId, apiClientOptions, account);
     await controller._exec(resourceId);
-  });
-
-  /*
-   * Check if password is powned
-   *
-   * @listens passbolt.secrets.powned-password
-   * @param requestId {uuid} The request identifier
-   * @param password {string} the password to check
-   */
-  worker.port.on('passbolt.secrets.powned-password', async(requestId, password) => {
-    const controller = new PownedPasswordController(worker, requestId);
-    await controller._exec(password);
   });
 };
 

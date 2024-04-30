d24aefa792c95638cc41dfd1d360a3abdbde3c16
Cedric Alfonsi <cedric@passbolt.com>
2024-04-30 12:43:33 +0000

    Apply 3 suggestion(s) to 1 file(s)
    Co-authored-by: Benjamin Monnot <benjamin@passbolt.com>

--- test/matchers/toThrowEntityValidationError.js
+++ test/matchers/toThrowEntityValidationError.js
@@ -1,15 +1,15 @@
 /**
  * Passbolt ~ Open source password manager for teams
- * Copyright (c) 2022 Passbolt SA (https://www.passbolt.com)
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
  *
  * Licensed under GNU Affero General Public License version 3 of the or any later version.
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright     Copyright (c) 2022 Passbolt SA (https://www.passbolt.com)
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
  * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
  * @link          https://www.passbolt.com Passbolt(tm)
- * @since         4.7.0
+ * @since         4.8.0
  */
 
 exports.toThrowEntityValidationError = function(received, propertyName, validationRule, dto) {

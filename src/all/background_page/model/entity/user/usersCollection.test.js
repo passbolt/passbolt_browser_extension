3fc8cb621458ca2de056815dd1508173612ddd3b
Cedric Alfonsi <cedric@passbolt.com>
2024-05-03 05:54:08 +0200

    PB-33227 - Migrate UsersCollection to v2 and cover user model sanitization with tests
    Signed-off-by: Cedric Alfonsi <cedric@passbolt.com>

--- src/all/background_page/model/entity/user/usersCollection.test.js
+++ src/all/background_page/model/entity/user/usersCollection.test.js
@@ -12,90 +12,195 @@
  * @since         2.13.0
  */
 import UsersCollection from "./usersCollection";
-import EntityCollectionError from "passbolt-styleguide/src/shared/models/entity/abstract/entityCollectionError";
-import {UsersCollectionTestFixtures} from "./usersCollection.test.fixtures";
 import EntitySchema from "passbolt-styleguide/src/shared/models/entity/abstract/entitySchema";
+import {defaultUserDto} from "passbolt-styleguide/src/shared/models/entity/user/userEntity.test.data";
+import UserEntity from "./userEntity";
+import {defaultGroupUser} from "passbolt-styleguide/src/shared/models/entity/groupUser/groupUserEntity.test.data.js";
 
-describe("User entity", () => {
+describe("UsersCollection", () => {
   it("schema must validate", () => {
     EntitySchema.validateSchema(UsersCollection.ENTITY_NAME, UsersCollection.getSchema());
   });
 
-  it("constructor works if valid minimal DTO is provided", () => {
-    const user1 = {
-      "id": "d57c10f5-639d-5160-9c81-8a0c6c4ec851",
-      "role_id": "0d51c3a8-5e67-5e3d-882f-e1868966d817",
-      "username": "user1@passbolt.com"
-    };
-    const user2 = {
-      "id": "d57c10f5-639d-5160-9c81-8a0c6c4ec852",
-      "role_id": "0d51c3a8-5e67-5e3d-882f-e1868966d817",
-      "username": "user2@passbolt.com"
-    };
-    const dto = [user1, user2];
-    const entity = new UsersCollection(dto);
-    expect(entity.toDto()).toEqual(dto);
-    expect(JSON.stringify(entity)).toEqual(JSON.stringify(dto));
-    expect(entity.items[0].username).toEqual('user1@passbolt.com');
-    expect(entity.items[1].username).toEqual('user2@passbolt.com');
-    expect(entity.ids).toEqual(["d57c10f5-639d-5160-9c81-8a0c6c4ec851", "d57c10f5-639d-5160-9c81-8a0c6c4ec852"]);
+  describe("UsersCollection::constructor", () => {
+    it("works with empty collection", () => {
+      new UsersCollection([]);
+    });
+
+    it("works if valid array of DTOs is provided", () => {
+      const dto1 = defaultUserDto({username: "user1@passbolt.com"});
+      const dto2 = defaultUserDto({username: "user2@passbolt.com"});
+      const dto3 = defaultUserDto({username: "user3@passbolt.com"});
+      const dtos = [dto1, dto2, dto3];
+      const collection = new UsersCollection(dtos);
+
+      expect.assertions(31);
+      expect(collection.items).toHaveLength(3);
+      expect(collection.items[0]).toBeInstanceOf(UserEntity);
+      expect(collection.items[0]._props.id).toEqual(dto1.id);
+      expect(collection.items[0]._props.username).toEqual(dto1.username);
+      expect(collection.items[0]._props.active).toEqual(dto1.active);
+      expect(collection.items[0]._props.deleted).toEqual(dto1.deleted);
+      expect(collection.items[0]._props.created).toEqual(dto1.created);
+      expect(collection.items[0]._props.modified).toEqual(dto1.modified);
+      expect(collection.items[0]._props.last_logged_in).toEqual(dto1.last_logged_in);
+      expect(collection.items[0]._props.is_mfa_enabled).toEqual(dto1.is_mfa_enabled);
+      expect(collection.items[0]._props.locale).toEqual(dto1.locale);
+      expect(collection.items[1]).toBeInstanceOf(UserEntity);
+      expect(collection.items[1]._props.id).toEqual(dto2.id);
+      expect(collection.items[1]._props.username).toEqual(dto2.username);
+      expect(collection.items[1]._props.active).toEqual(dto2.active);
+      expect(collection.items[1]._props.deleted).toEqual(dto2.deleted);
+      expect(collection.items[1]._props.created).toEqual(dto2.created);
+      expect(collection.items[1]._props.modified).toEqual(dto2.modified);
+      expect(collection.items[1]._props.last_logged_in).toEqual(dto2.last_logged_in);
+      expect(collection.items[1]._props.is_mfa_enabled).toEqual(dto2.is_mfa_enabled);
+      expect(collection.items[1]._props.locale).toEqual(dto2.locale);
+      expect(collection.items[2]).toBeInstanceOf(UserEntity);
+      expect(collection.items[2]._props.id).toEqual(dto3.id);
+      expect(collection.items[2]._props.username).toEqual(dto3.username);
+      expect(collection.items[2]._props.active).toEqual(dto3.active);
+      expect(collection.items[2]._props.deleted).toEqual(dto3.deleted);
+      expect(collection.items[2]._props.created).toEqual(dto3.created);
+      expect(collection.items[2]._props.modified).toEqual(dto3.modified);
+      expect(collection.items[2]._props.last_logged_in).toEqual(dto3.last_logged_in);
+      expect(collection.items[2]._props.is_mfa_enabled).toEqual(dto3.is_mfa_enabled);
+      expect(collection.items[2]._props.locale).toEqual(dto3.locale);
+    });
+
+    it("works if valid user entities is provided", () => {
+      const entity = new UserEntity(defaultUserDto());
+      const dtos = [entity];
+      const collection = new UsersCollection(dtos);
+
+      expect.assertions(11);
+      expect(collection.items).toHaveLength(1);
+      expect(collection.items[0]).toBeInstanceOf(UserEntity);
+      expect(collection.items[0]._props.id).toEqual(entity._props.id);
+      expect(collection.items[0]._props.username).toEqual(entity._props.username);
+      expect(collection.items[0]._props.active).toEqual(entity._props.active);
+      expect(collection.items[0]._props.deleted).toEqual(entity._props.deleted);
+      expect(collection.items[0]._props.created).toEqual(entity._props.created);
+      expect(collection.items[0]._props.modified).toEqual(entity._props.modified);
+      expect(collection.items[0]._props.last_logged_in).toEqual(entity._props.last_logged_in);
+      expect(collection.items[0]._props.is_mfa_enabled).toEqual(entity._props.is_mfa_enabled);
+      expect(collection.items[0]._props.locale).toEqual(entity._props.locale);
+    });
+
+    it("should throw if one of data item does not validate the collection entity schema", () => {
+      const dto1 = defaultUserDto();
+      const dto2 = defaultUserDto({username: 42});
+
+      expect.assertions(1);
+      expect(() => new UsersCollection([dto1, dto2]))
+        .toThrowCollectionValidationError("1.username.type");
+    });
+
+    /*
+     * @todo Associated entities validation error details to review when collection will aggregate them.
+     * @see EntityV2Collection.pushMany
+     */
+    it("should throw if one of data item does not validate the collection associated entity schema", () => {
+      const dto1 = defaultUserDto();
+      const dto2 = defaultUserDto({
+        groups_users: [
+          defaultGroupUser({group_id: 42, is_admin: true})
+        ]
+      });
+
+      expect.assertions(2);
+      // Should not throw
+      expect(() => new UsersCollection([dto1, dto2]))
+        .toThrowCollectionValidationError("1.0.group_id.type");
+      // Should throw
+      expect(() => new UsersCollection([dto1, dto2]))
+        .not.toThrowCollectionValidationError("1.groups_users.0.group_id.type");
+    });
+
+    it("should throw if one of data item does not validate the unique id build rule", () => {
+      const dto1 = defaultUserDto({username: "user1@passbolt.com"});
+      const dto2 = defaultUserDto({id: dto1.id, username: "user2@passbolt.com"});
+      const dto3 = defaultUserDto({username: "user3@passbolt.com"});
+
+      expect.assertions(1);
+      expect(() => new UsersCollection([dto1, dto2, dto3]))
+        .toThrowCollectionValidationError("1.id.unique");
+    });
+
+    it("should throw if one of data item does not validate the unique username build rule", () => {
+      const dto1 = defaultUserDto({username: "user1@passbolt.com"});
+      const dto2 = defaultUserDto({username: dto1.username});
+      const dto3 = defaultUserDto({username: "user3@passbolt.com"});
+
+      expect.assertions(1);
+      expect(() => new UsersCollection([dto1, dto2, dto3]))
+        .toThrowCollectionValidationError("1.username.unique");
+    });
+
+    it("should, with enabling the ignore invalid option, ignore items which do not validate their schema", () => {
+      const dto1 = defaultUserDto();
+      const dto2 = defaultUserDto({username: 42});
+
+      expect.assertions(2);
+      const collection = new UsersCollection([dto1, dto2], {ignoreInvalidEntity: true});
+      expect(collection.items).toHaveLength(1);
+      expect(collection.items[0].id).toEqual(dto1.id);
+    });
+
+    it("should, with enabling the ignore invalid option, ignore items which do not validate the unique id build rule", () => {
+      const dto1 = defaultUserDto({username: "user1@passbolt.com"});
+      const dto2 = defaultUserDto({id: dto1.id, username: "user2@passbolt.com"});
+
+      expect.assertions(2);
+      const collection = new UsersCollection([dto1, dto2], {ignoreInvalidEntity: true});
+      expect(collection.items).toHaveLength(1);
+      expect(collection.items[0].id).toEqual(dto1.id);
+    });
+
+    it("should, with enabling the ignore invalid option, ignore items which do not validate the unique username build rule", () => {
+      const dto1 = defaultUserDto({username: "user1@passbolt.com"});
+      const dto2 = defaultUserDto({username: dto1.username});
+
+      expect.assertions(2);
+      const collection = new UsersCollection([dto1, dto2], {ignoreInvalidEntity: true});
+      expect(collection.items).toHaveLength(1);
+      expect(collection.items[0].id).toEqual(dto1.id);
+    });
+
+    it("should, with enabling the ignore invalid option, ignore items associated groups users entities which do not validate the group users user_id schema validation", () => {
+      const dto1 = defaultUserDto({username: "user1@passbolt.com"}, {withGroupsUsers: true});
+      const dto2 = defaultUserDto({
+        username: "user2@passbolt.com",
+        groups_users: [
+          defaultGroupUser({user_id: 42, is_admin: true})
+        ]
+      });
+      const dto3 = defaultUserDto({username: "user3@passbolt.com"}, {withGroupsUsers: true});
+
+      expect.assertions(7);
+      const collection = new UsersCollection([dto1, dto2, dto3], {ignoreInvalidEntity: true});
+      expect(collection.items).toHaveLength(3);
+      expect(collection.items[0].id).toEqual(dto1.id);
+      expect(collection.items[0]._groups_users).toHaveLength(1);
+      expect(collection.items[1].id).toEqual(dto2.id);
+      expect(collection.items[1]._groups_users).toHaveLength(0);
+      expect(collection.items[2].id).toEqual(dto3.id);
+      expect(collection.items[2]._groups_users).toHaveLength(1);
+    });
   });
 
-  it("constructor fails if reusing same user", () => {
-    const user1 = {
-      "id": "d57c10f5-639d-5160-9c81-8a0c6c4ec851",
-      "role_id": "0d51c3a8-5e67-5e3d-882f-e1868966d817",
-      "username": "user1@passbolt.com"
-    };
-    const dto = [user1, user1];
+  describe("UsersCollection:toDto", () => {
+    it("should transform the collection items in dto format", () => {
+      const dto1 = defaultUserDto({username: "user1@passbolt.com"});
+      const dto2 = defaultUserDto({username: "user2@passbolt.com"});
+      const dto3 = defaultUserDto({username: "user3@passbolt.com"});
+      const dtos = [dto1, dto2, dto3];
+      const collection = new UsersCollection(dtos);
 
-    const t = () => { new UsersCollection(dto); };
-    expect(t).toThrow(EntityCollectionError);
-  });
-
-  it("constructor fails if reusing same id", () => {
-    const user1 = {
-      "id": "d57c10f5-639d-5160-9c81-8a0c6c4ec851",
-      "role_id": "0d51c3a8-5e67-5e3d-882f-e1868966d817",
-      "username": "user1@passbolt.com"
-    };
-    const user2 = {
-      "id": "d57c10f5-639d-5160-9c81-8a0c6c4ec851",
-      "role_id": "0d51c3a8-5e67-5e3d-882f-e1868966d817",
-      "username": "user2@passbolt.com"
-    };
-    const dto = [user1, user2];
-
-    const t = () => { new UsersCollection(dto); };
-    expect(t).toThrow(EntityCollectionError);
-  });
-
-  it("constructor fails if reusing same username", () => {
-    const user1 = {
-      "id": "d57c10f5-639d-5160-9c81-8a0c6c4ec851",
-      "role_id": "0d51c3a8-5e67-5e3d-882f-e1868966d817",
-      "username": "user1@passbolt.com"
-    };
-    const user2 = {
-      "id": "d57c10f5-639d-5160-9c81-8a0c6c4ec852",
-      "role_id": "0d51c3a8-5e67-5e3d-882f-e1868966d817",
-      "username": "user1@passbolt.com"
-    };
-    const dto = [user1, user2];
-
-    const t = () => { new UsersCollection(dto); };
-    expect(t).toThrow(EntityCollectionError);
-  });
-
-  it("constructor works with empty collection", () => {
-    const collection = new UsersCollection([]);
-    expect(collection.ids).toEqual([]);
-  });
-
-  it("serialization works with full object inside collection", () => {
-    const dto = UsersCollectionTestFixtures.default;
-    const collection = new UsersCollection(dto);
-    expect(collection.toDto()).toEqual(dto);
+      expect.assertions(2);
+      expect(collection.toDto()).toEqual(dtos);
+      expect(JSON.stringify(collection)).toEqual(JSON.stringify(dtos));
+    });
   });
 
   describe("sanitizeDto", () => {

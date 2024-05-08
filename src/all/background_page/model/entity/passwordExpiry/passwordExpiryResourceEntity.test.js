1ee8bde6ee4a2a7a321b8a5db1a2844e3982ccdd
Stéphane Loegel <stephane@passbolt.com>
2024-05-08 15:41:02 +0200

    PB-33264: add coverage on PasswordExpiryResourceEntity

--- src/all/background_page/model/entity/passwordExpiry/passwordExpiryResourceEntity.test.js
+++ src/all/background_page/model/entity/passwordExpiry/passwordExpiryResourceEntity.test.js
@@ -11,17 +11,55 @@
  * @link          https://www.passbolt.com Passbolt(tm)
  * @since         4.5.0
  */
-
-import each from "jest-each";
 import EntitySchema from "passbolt-styleguide/src/shared/models/entity/abstract/entitySchema";
-import EntityValidationError from "passbolt-styleguide/src/shared/models/entity/abstract/entityValidationError";
 import PasswordExpiryResourceEntity from "./passwordExpiryResourceEntity";
 import {defaultPasswordExpiryResourceDto} from "./passwordExpiryResourceEntity.test.data";
 import {v4 as uuid} from "uuid";
+import * as assertEntityProperty from "passbolt-styleguide/test/assert/assertEntityProperty";
 
 describe("PasswordExpiryResource entity", () => {
-  it("schema must validate", () => {
-    EntitySchema.validateSchema(PasswordExpiryResourceEntity.ENTITY_NAME, PasswordExpiryResourceEntity.getSchema());
+  describe("PasswordExpiryResourceEntity::getSchema", () => {
+    it("schema must validate", () => {
+      EntitySchema.validateSchema(PasswordExpiryResourceEntity.ENTITY_NAME, PasswordExpiryResourceEntity.getSchema());
+    });
+
+    it("validates id property", () => {
+      assertEntityProperty.uuid(PasswordExpiryResourceEntity, "id");
+      assertEntityProperty.required(PasswordExpiryResourceEntity, "id");
+    });
+
+    it("validates expires property", () => {
+      const successScenarios = [
+        ...assertEntityProperty.SUCCESS_DATETIME_SCENARIOS,
+        assertEntityProperty.SCENARIO_NULL,
+      ];
+      const failingScenarios = assertEntityProperty.FAIL_DATETIME_SCENARIOS;
+
+      assertEntityProperty.assert(PasswordExpiryResourceEntity, "expired", successScenarios, failingScenarios, "type");
+      assertEntityProperty.required(PasswordExpiryResourceEntity, "expired");
+    });
+
+    it("validates created property", () => {
+      assertEntityProperty.string(PasswordExpiryResourceEntity, "created");
+      assertEntityProperty.dateTime(PasswordExpiryResourceEntity, "created");
+      assertEntityProperty.notRequired(PasswordExpiryResourceEntity, "created");
+    });
+
+    it("validates modified property", () => {
+      assertEntityProperty.string(PasswordExpiryResourceEntity, "modified");
+      assertEntityProperty.dateTime(PasswordExpiryResourceEntity, "modified");
+      assertEntityProperty.notRequired(PasswordExpiryResourceEntity, "modified");
+    });
+
+    it("validates created_by property", () => {
+      assertEntityProperty.uuid(PasswordExpiryResourceEntity, "created_by");
+      assertEntityProperty.notRequired(PasswordExpiryResourceEntity, "created_by");
+    });
+
+    it("validates modified_by property", () => {
+      assertEntityProperty.uuid(PasswordExpiryResourceEntity, "modified_by");
+      assertEntityProperty.notRequired(PasswordExpiryResourceEntity, "modified_by");
+    });
   });
 
   it("should accept a mininal valid DTO", () => {
@@ -47,58 +85,5 @@ describe("PasswordExpiryResource entity", () => {
 
     const entity = new PasswordExpiryResourceEntity(expectedDto);
     expect(entity.toDto()).toStrictEqual(expectedDto);
-  });
-
-  it("should throw an exception if required fields are not present", () => {
-    const requiredFieldNames = PasswordExpiryResourceEntity.getSchema().required;
-    const requiredFieldCount = 2;
-    expect.assertions(requiredFieldCount * 2 + 1);
-
-    expect(requiredFieldNames.length).toStrictEqual(requiredFieldCount);
-
-    for (let i = 0; i < requiredFieldNames.length; i++) {
-      const fieldName = requiredFieldNames[i];
-      const dto = defaultPasswordExpiryResourceDto();
-      delete dto[fieldName];
-      try {
-        new PasswordExpiryResourceEntity(dto);
-      } catch (e) {
-        expect(e).toBeInstanceOf(EntityValidationError);
-        expect(e.hasError(fieldName, "required")).toStrictEqual(true);
-      }
-    }
-  });
-
-  each([
-    {dto: {id: "string but not uuid"}, errorType: "format"},
-    {dto: {id: -1}, errorType: "type"},
-
-    {dto: {expired: "string but not a date"}, errorType: "type"},
-    {dto: {expired: -1}, errorType: "type"},
-
-    {dto: {created: "string but not a date"}, errorType: "format"},
-    {dto: {created: -1}, errorType: "type"},
-
-    {dto: {created_by: "string but not uuid"}, errorType: "format"},
-    {dto: {created_by: -1}, errorType: "type"},
-
-    {dto: {modified: "string but not a date"}, errorType: "format"},
-    {dto: {modified: -1}, errorType: "type"},
-
-    {dto: {modified_by: "string but not uuid"}, errorType: "format"},
-    {dto: {modified_by: -1}, errorType: "type"},
-  ]).describe("should throw an exception if DTO contains invalid values", scenario => {
-    it(`scenario: ${JSON.stringify(scenario)}`, () => {
-      expect.assertions(2);
-      const fieldName = Object.keys(scenario.dto)[0];
-      const erroneousDto = defaultPasswordExpiryResourceDto(scenario.dto);
-
-      try {
-        new PasswordExpiryResourceEntity(erroneousDto);
-      } catch (e) {
-        expect(e).toBeInstanceOf(EntityValidationError);
-        expect(e.hasError(fieldName, scenario.errorType)).toStrictEqual(true);
-      }
-    });
   });
 });

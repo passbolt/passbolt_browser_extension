0cf48957d0fba406ef35dee98afa0ba1060a8967
Benjamin Monnot <benjamin@passbolt.com>
2024-05-10 11:21:24 +0200

    PB-33321 Fix local storage loading on extension update

--- src/all/background_page/controller/extension/onExtensionInstalledController.js
+++ src/all/background_page/controller/extension/onExtensionInstalledController.js
@@ -21,6 +21,8 @@ import CheckAuthStatusService from "../../service/auth/checkAuthStatusService";
 import User from "../../model/user";
 import Log from "../../model/log";
 import {BrowserExtensionIconService} from "../../service/ui/browserExtensionIcon.service";
+import storage from "../../sdk/storage";
+import {Config} from "../../model/config";
 
 class OnExtensionInstalledController {
   /**
@@ -29,9 +31,13 @@ class OnExtensionInstalledController {
    * @returns {Promise<void>}
    */
   static async exec(details) {
-    console.debug(`The install reason is: ${details.reason}`);
-    console.debug(`The previous version is: ${details.previousVersion}`);
-    console.debug(`The shared_module_update id is: ${details.id}`);
+    // Check if the storage have some data
+    if (Object.keys(storage._data).length === 0) {
+      // Fix the initialization of the storage after an update
+      await storage.init();
+      // Initialization of the config to get the user information
+      Config.init();
+    }
     switch (details.reason) {
       case browser.runtime.OnInstalledReason.INSTALL:
         await OnExtensionInstalledController.onInstall();

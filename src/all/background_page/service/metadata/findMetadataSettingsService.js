13bfaa34dd0a22922c262e68410c717382343b1a
Cedric Alfonsi <cedric@passbolt.com>
2024-10-04 06:29:01 +0000

    WP3-4.3 Implement findTypesSettings on FindMetadataSettingsService to retrieve metadata types settings entity

---
+++ src/all/background_page/service/metadata/findMetadataSettingsService.js
@@ -0,0 +1,38 @@
+/**
+ * Passbolt ~ Open source password manager for teams
+ * Copyright (c) Passbolt SA (https://www.passbolt.com)
+ *
+ * Licensed under GNU Affero General Public License version 3 of the or any later version.
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Passbolt SA (https://www.passbolt.com)
+ * @license       https://opensource.org/licenses/AGPL-3.0 AGPL License
+ * @link          https://www.passbolt.com Passbolt(tm)
+ * @since         4.10.0
+ */
+import MetadataTypesSettingsApiService from "../api/metadata/metadataTypesSettingsApiService";
+import MetadataTypesSettingsEntity
+  from "passbolt-styleguide/src/shared/models/entity/metadata/metadataTypesSettingsEntity";
+
+/**
+ * The service aims to find resources from the API.
+ */
+export default class FindMetadataSettingsService {
+  /**
+   * @constructor
+   * @param {ApiClientOptions} apiClientOptions The api client options
+   */
+  constructor(apiClientOptions) {
+    this.metadataTypesSettingsApiService = new MetadataTypesSettingsApiService(apiClientOptions);
+  }
+
+  /**
+   * Retrieve the metadata types settings.
+   * @returns {Promise<MetadataTypesSettingsEntity>}
+   */
+  async findTypesSettings() {
+    const dto = await this.metadataTypesSettingsApiService.findSettings();
+    return new MetadataTypesSettingsEntity(dto);
+  }
+}

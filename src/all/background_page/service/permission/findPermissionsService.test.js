9132c99461f80514d78ce25b35c0197678c8b8fc
Pierre Colart <pierre@passbolt.com>
2025-11-03 08:47:10 +0000

    PB-46065 - Prevent re-encryption of metadata with personal user key when...

--- src/all/background_page/service/permission/findPermissionsService.test.js
+++ src/all/background_page/service/permission/findPermissionsService.test.js
@@ -20,19 +20,19 @@ import {defaultAccountDto} from "../../model/entity/account/accountEntity.test.d
 import {ownerPermissionDto} from "passbolt-styleguide/src/shared/models/entity/permission/permissionEntity.test.data.js";
 
 describe("FindPermissionService", () => {
-  describe("FindPermissionService::exec", () => {
+  describe("::findAllByAcoForeignKeyForDisplay", () => {
     it("Should call permission service to find all permission from a resource id", async() => {
       expect.assertions(3);
-      // initialisation
+
       const account = new AccountEntity(defaultAccountDto());
       const service = new FindPermissionsService(account, defaultApiClientOptions());
       const resourceId = uuidv4();
       const expectedDto = [ownerPermissionDto()];
-      // mocked function
+
       jest.spyOn(service.permissionService, "findAllByAcoForeignKey").mockImplementationOnce(() => expectedDto);
-      // process
+
       const permissionsCollection = await service.findAllByAcoForeignKeyForDisplay(resourceId);
-      // expectations
+
       expect(service.permissionService.findAllByAcoForeignKey).toHaveBeenCalledTimes(1);
       expect(service.permissionService.findAllByAcoForeignKey).toHaveBeenCalledWith(resourceId, FindPermissionsService.DEFAULT_CONTAIN);
       expect(permissionsCollection.toDto()).toStrictEqual(expectedDto);
@@ -40,16 +40,60 @@ describe("FindPermissionService", () => {
 
     it("Should fail if the resource id is not a uuid", async() => {
       expect.assertions(1);
-      // initialisation
+
       const account = new AccountEntity(defaultAccountDto());
       const service = new FindPermissionsService(account, defaultApiClientOptions());
       const resourceId = "not a uuid";
       try {
-        // process
         await service.findAllByAcoForeignKeyForDisplay(resourceId);
       } catch (error) {
-        // expectations
         expect(error.message).toBe(`Service error. The id '${resourceId}' is not a valid uuid.`);
+      }
+    });
+  });
+
+  describe("::findAllByAcoForeignKey", () => {
+    it("Should call permission service to find all permission from a resource id without contain", async() => {
+      expect.assertions(3);
+      const account = new AccountEntity(defaultAccountDto());
+      const service = new FindPermissionsService(account, defaultApiClientOptions());
+      const resourceId = uuidv4();
+      const expectedDto = [ownerPermissionDto()];
+      jest.spyOn(service.permissionService, "findAllByAcoForeignKey").mockImplementationOnce(() => expectedDto);
+      const permissionsCollection = await service.findAllByAcoForeignKey(resourceId);
+
+      expect(service.permissionService.findAllByAcoForeignKey).toHaveBeenCalledTimes(1);
+      expect(service.permissionService.findAllByAcoForeignKey).toHaveBeenCalledWith(resourceId);
+      expect(permissionsCollection.toDto()).toStrictEqual(expectedDto);
+    });
+
+    it("Should fail if the resource id is not a uuid", async() => {
+      expect.assertions(1);
+      const account = new AccountEntity(defaultAccountDto());
+      const service = new FindPermissionsService(account, defaultApiClientOptions());
+      const resourceId = "not a uuid";
+      try {
+        await service.findAllByAcoForeignKey(resourceId);
+      } catch (error) {
+        expect(error.message).toBe(`Service error. The id '${resourceId}' is not a valid uuid.`);
+      }
+    });
+
+    it("Should fail if the permission service returns an error 500", async() => {
+      expect.assertions(1);
+      const account = new AccountEntity(defaultAccountDto());
+      const service = new FindPermissionsService(account, defaultApiClientOptions());
+      const resourceId = uuidv4();
+      const expectedError = new Error("Internal Server Error");
+
+      jest.spyOn(service.permissionService, "findAllByAcoForeignKey").mockImplementationOnce(() => {
+        throw expectedError;
+      });
+
+      try {
+        await service.findAllByAcoForeignKey(resourceId);
+      } catch (error) {
+        expect(error.message).toBe("Internal Server Error");
       }
     });
   });
